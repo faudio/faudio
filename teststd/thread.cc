01e0aa15ef8bc9121559c1319a5894cf5914c02d
Hans Höglund <hans@hanshoglund.se>
2012-07-28 00:13:07 +0200

    Switch between std and boost

--- teststd/thread.cc
+++ teststd/thread.cc
@@ -1,17 +1,24 @@
 
-#include <thread>
-#include <boost/thread.hpp>
-#include <mutex>
-#include <condition_variable>
-#include <atomic>
+#define USE_BOOST_THREAD
+
+#ifndef USE_BOOST_THREAD
+  #include <thread>
+  #include <condition_variable>
+  #include <atomic>
+  #define T std
+#else
+  #include <boost/thread.hpp>
+  #define T boost
+#endif
+
 #include <iostream>
 
-static boost::mutex foo_bar_mutex;
+static T::mutex foo_bar_mutex;
 void foo()
 {
     for(int i = 0; i < 10; ++i)
     {
-        boost::lock_guard<boost::mutex> foo_bar_lock (foo_bar_mutex);
+        T::lock_guard<T::mutex> foo_bar_lock (foo_bar_mutex);
         std::cout << "This is foo\n";
     }
 }
@@ -19,14 +26,14 @@ void bar()
 {
     for(int i = 0; i < 10; ++i)
     {
-        boost::lock_guard<boost::mutex> foo_bar_lock (foo_bar_mutex);
+        T::lock_guard<T::mutex> foo_bar_lock (foo_bar_mutex);
         std::cout << "This is bar\n";
     }
 }
 int two_threads()
 {
-    boost::thread x (foo);
-    boost::thread y (bar);
+    T::thread x (foo);
+    T::thread y (bar);
     x.join();
     y.join();
     return 0;
