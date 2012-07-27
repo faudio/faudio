111306563e747a99c61f1c920fb940e60f8e411a
Hans Höglund <hans@hanshoglund.se>
2012-07-28 00:04:10 +0200

    - Boost in place of C++0x threads

--- teststd/thread.cc
+++ teststd/thread.cc
@@ -1,16 +1,17 @@
 
 #include <thread>
+#include <boost/thread.hpp>
 #include <mutex>
 #include <condition_variable>
 #include <atomic>
 #include <iostream>
 
-static std::mutex foo_bar_mutex;
+static boost::mutex foo_bar_mutex;
 void foo()
 {
     for(int i = 0; i < 10; ++i)
     {
-        std::lock_guard<std::mutex> foo_bar_lock (foo_bar_mutex);
+        boost::lock_guard<boost::mutex> foo_bar_lock (foo_bar_mutex);
         std::cout << "This is foo\n";
     }
 }
@@ -18,14 +19,14 @@ void bar()
 {
     for(int i = 0; i < 10; ++i)
     {
-        std::lock_guard<std::mutex> foo_bar_lock (foo_bar_mutex);
+        boost::lock_guard<boost::mutex> foo_bar_lock (foo_bar_mutex);
         std::cout << "This is bar\n";
     }
 }
 int two_threads()
 {
-    std::thread x (foo);
-    std::thread y (bar);
+    boost::thread x (foo);
+    boost::thread y (bar);
     x.join();
     y.join();
     return 0;
