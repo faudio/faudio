7b1b9ec2584136e3c43fdf03de4d2254b1519ed7
Hans Höglund <hans@hanshoglund.se>
2012-08-02 00:29:44 +0200

    - Ported reference

--- scl/reference/reference.hpp
+++ scl/reference/reference.hpp
@@ -3,6 +3,32 @@ namespace scl
 {
   namespace immutable
   {
+    // Reference counted mixin
+    template <class Self>
+    class reference_counted
+    {
+    public:
+      reference_counted() : reference_count(0) {}
+      ~reference_counted() {}
+      Self retain();
+      void release();
+    private:
+      int reference_count;
+    };
   }
 }
+
+#ifndef SCL_REFERENCE_NO_BOOST
+namespace boost
+{
+  void intrusive_ptr_add_ref(reference_counted* p)
+  {
+    p.retain();
+  }
+  void intrusive_ptr_release(reference_counted* p)
+  {
+    p.release();
+  }
+}
+#endif
 
