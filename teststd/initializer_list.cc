e7a97cab0badec8612e43fd12dc8185c5e75a1bf
Hans Höglund <hans@hanshoglund.se>
2012-08-01 15:25:41 +0200

    - Initializer list - Exception pointer

--- teststd/initializer_list.cc
+++ teststd/initializer_list.cc
@@ -1,13 +1,35 @@
 
 #include <iostream>
+#include <initializer_list>
 
-int test_foo()
+template <class T>
+struct foo
 {
+  foo(std::initializer_list<T> args)
+  {
+    std::cout << "Called with " << args.size() << " uniform arguments.\n";
+    for(T arg : args)
+      std::cout << "  " << arg << "\n";
+  }
+  foo(int x, double y, const char* z)
+  {
+    std::cout << "Called with 3 different arguments.\n";
+    std::cout << "  " << x << "\n";
+    std::cout << "  " << y << "\n";
+    std::cout << "  " << z << "\n";
+  }
+};
+
+
+int test_init_list()
+{
+  foo<int> xs = { 1, 2, 3 };
+  foo<int> ys = { 1, 2, "My name is Hans!" };
 }   
 
 int main(int argc, char const *argv[])
 {
   return 0
-     || test_foo()
+     || test_init_list()
   ;
 }
