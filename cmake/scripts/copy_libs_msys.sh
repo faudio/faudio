d8b56f02cf9261c0042f40f7c719175604968b99
Hans Höglund <hans@hanshoglund.se>
2012-08-01 19:01:49 +0200

    - Added missing script

---
+++ cmake/scripts/copy_libs_msys.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp external_libraries/fluidsynth/glib/msys/bin/libglib-2.0-0.dll build/bin;
+cp external_libraries/fluidsynth/glib/msys/bin/libgthread-2.0-0.dll build/bin;
+cp /c/mingw/bin/libgcc_s_dw2-1.dll build/bin;
+cp /c/mingw/bin/libstdc++-6.dll build/bin;
