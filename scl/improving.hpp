eedcb55c704293bf6d143ad0d5e6a5e99d41fd1e
Hans Höglund <hans@hanshoglund.se>
2012-08-10 18:21:40 +0200

    - Added wakeup_service

--- scl/improving.hpp
+++ scl/improving.hpp
@@ -1,6 +1,8 @@
 
 #pragma once
 
+#include <map>
+#include <iostream>
 #include <scl/atomic.hpp>
 #include <scl/thread.hpp>
 #include <scl/concept.hpp>
@@ -17,99 +19,186 @@ namespace scl
       the value becomes fixed. As with std::future, wait blocks forever if this does not happen.
 
   */
-  template <class T>
-  class improving
+//   template <class T>
+//   class improving
+//   {
+//   public:
+// #ifdef DOXYGEN_SHOULD_SKIP_THIS
+//     BOOST_CONCEPT_ASSERT((LowerBound<T>));
+//     BOOST_CONCEPT_ASSERT((LessThanComparable<T>));
+// #endif
+//     using value_type = T;
+//     using this_type  = improving<T>;
+//
+//     improving() noexcept;        // Fixed at std::numeric_limits::lowest()
+//     improving(const T) noexcept; // Fixed at the given value
+//     improving(this_type &&);
+//     ~improving() noexcept;
+//
+//     void operator =(improving &&);
+//     void swap(this_type& other);
+//
+//     bool known() const noexcept;
+//     T value() const;
+//     void wait() const;
+//     thread::future<T> to_future() const;
+//
+//   private:
+//     // accumulator
+//   };
+//
+//   template <class T>
+//   improving<T>::improving()
+//     : current(std::numeric_limits<T>::lowest()) {}
+//
+//   /*
+//     wait()
+//     {
+//       block on cond var
+//        wake up if (current > max)
+//     }
+//
+//     known()
+//     {
+//       current.get() == max.get();
+//     }
+//
+//     min(x, y)
+//     {
+//       return improving(
+//         max(this->current, other.current),
+//         max(this->max, other.max)
+//         accumulator
+//         );
+//     }
+//   */
+
+  // /**
+  //     The improving analogue to a promise.Can improve or fix the improving value, or set an
+  //     exception that will be transported to the reading thread.
+  //  */
+  // template <class T>
+  // class accumulator
+  // {
+  // public:
+  //   improving<T> get_improving() const;
+  //
+  //   void fix()
+  //   {
+  //     max.exchange(current.get());
+  //     wakeup.notify();
+  //   }
+  //   void increment(const T& amount)
+  //   {
+  //     // Can be specialized for AtomicIntegral types
+  //     // increment atomically
+  //   }
+  //   void increment_and_fix(const T& amount)
+  //   {
+  //     // atomically
+  //       // increment
+  //       // max := current
+  //     wakeup.notify();
+  //   }
+  //
+  // private:
+  //   atomic::atomic<T> current;
+  //   atomic::atomic<T> max;
+  //   wakeup_service() wakeup;
+  // };
+
+  // sleep :: (Event -> Bool) -> ()
+  // wake  :: Event -> ()
+
+  namespace detail
+  {
+    template <class Event>
+    struct wakeup_service_state
+    {      
+      wakeup_service_state()
+        : tested(0) {}
+      using event_type = Event;
+      event_type event;
+      thread::mutex mutex;
+      thread::condition_variable condition;
+      atomic::atomic_int blocked; // number of threads blocking
+      atomic::atomic_int tested; // number of threads waiting to read update condition
+    };
+  }
+
+  /** Provides composable blocking
+
+      Similar to a condition variable, with the difference that a thread may block on
+      a series of wakeups.
+      
+      Construction, destruction and moving is not thread-safe.
+    */
+  template <class Event>
+  class wakeup_service
   {
   public:
-#ifdef DOXYGEN_SHOULD_SKIP_THIS
-    BOOST_CONCEPT_ASSERT((LowerBound<T>));
-    BOOST_CONCEPT_ASSERT((LessThanComparable<T>));
-#endif
-    using value_type = T;
-    using this_type  = improving<T>;
+    BOOST_CONCEPT_ASSERT((DefaultConstructible<Event>));
+    BOOST_CONCEPT_ASSERT((Copyable<Event>));
+    using event_type = Event;
+    using this_type  = wakeup_service<event_type>;
+    using predicate_type = std::function<bool(event_type)>;
+    using state_type = detail::wakeup_service_state<event_type>;
 
-    improving() noexcept;        // Fixed at std::numeric_limits::lowest()
-    improving(const T) noexcept; // Fixed at the given value
-    improving(this_type &&);
-    ~improving() noexcept;
+    wakeup_service()
+      : state(new state_type) {}
+    wakeup_service(this_type&& other)
+      : state(std::move(other.state)) {}
 
-    void operator =(improving &&);
-    void swap(this_type& other);
+    void operator =(this_type&& other)
+    {
+      state = std::move(other.state);
+    }   
+    void swap(this_type& other)
+    {
+      std::swap(state, other.state);
+    }
 
-    bool known() const noexcept;
-    T value() const;
-    void wait() const;
-    thread::future<T> to_future() const;
+    // Block until an event mathcing the predicate occurs
+    void sleep(std::function<bool(event_type)> predicate)
+    {
+      using thread::mutex;
+      using thread::unique_lock;
+      {
+        unique_lock<mutex> lock (state->mutex);
+        state->blocked += 1;
+        do
+        {
+          state->condition.wait(lock);
+          // std::cout << "checking value: " << state->event << "\n";
+          // std::cout << "  tested is:    " << state->tested << "\n";
+          // std::cout << "  blocked is:   " << state->blocked << "\n";
+          state->tested += 1;
+        }
+        while (!predicate(state->event));
+        state->blocked -= 1;
+      }
+    }
 
-  private:
-    atomic::atomic<T> current;
-    atomic::atomic<T> max;
-    // cond var
-    // provider
+    // Signal the given event
+    void wake(event_type event)
+    {
+      using thread::mutex;
+      using thread::unique_lock;
+      // block until all threads have read
+      {                              
+        unique_lock<mutex> lock (state->mutex);
+        state->event = event;
+        state->tested = 0;
+        // std::cout << "sending value: " << state->event << "\n";
+        // std::cout << "  tested is:    " << state->tested << "\n";
+        // std::cout << "  blocked is:   " << state->blocked << "\n";
+      }
+      state->condition.notify_all();
+      while (state->tested < state->blocked); // busy-wait until the wake has been propagated
+    }
+
+  private:                                  
+    std::unique_ptr<state_type> state;
   };
-
-  template <class T>
-  improving<T>::improving()
-    : current(std::numeric_limits<T>::lowest()) {}
-
-  /*
-    wait()
-    {
-      block on cond var
-       wake up if (current > max)
-    }
-
-    known()
-    {
-      current.get() == max.get();
-    }
-
-    min(x, y)
-    {
-      return improving(
-        max(this->current, other.current),
-        max(this->max, other.max)
-        provider
-        );
-    }
-  */
-
-
-
-  /**
-      The improving analogue to a promise.Can improve or fix the improving value, or set an
-      exception that will be transported to the reading thread.
-   */
-  template <class T>
-  class provider
-  {
-    improving<T> get_improving();
-
-    void fix();
-    void increment(const T& amount);
-    void increment_and_fix(const T& amount);
-
-    void fix_at_thread_exit();
-    void increment_at_thread_exit(const T& amount);
-    void increment_and_fix_at_thread_exit(const T& amount);
-
-    void set_exception(std::exception_ptr ptr);
-    void set_exception_at_thread_exit(std::exception_ptr ptr);
-
-  private:
-    // list of improving
-  };
-
-  // // increment increments atomic var
-  // fix ()
-  // {
-  //   // may only be called once
-  //   // signal on first cond var
-  // }
-  // inc ()
-  // {
-  //   // add to atomic var
-  //   // signal on first cond var
-  // }
 
 }
--- test/improving/improving.cc
+++ test/improving/improving.cc
@@ -1,14 +1,114 @@
 
+#include <iostream>
+#include <chrono>
 #include <gtest/gtest.h>
 #include <scl/improving.hpp>
 
-using namespace scl;
-
-BOOST_CONCEPT_ASSERT((Moveable<improving<int>>));
-  // EqualityComparable<improving<T>>
-  // Monoid<improving<T>>
-
-TEST(Improving, Basic)
+namespace scl
 {
+  BOOST_CONCEPT_ASSERT((Moveable<wakeup_service<int>>));    
+
+  enum signal 
+  {
+    foo, bar, baz
+  };
+  
+  static thread::mutex print_mutex;
+  static atomic::atomic_int count (0);
+           
+  void sleep_until(wakeup_service<signal>* wakeup, signal sig)
+  {
+  
+    std::function<bool(signal)> pred = 
+      [=] (signal event) { return event == sig; };
+
+    wakeup->sleep(pred);
+
+    count++;
+    // {
+    //   thread::unique_lock<thread::mutex>(print_mutex);
+    //   std::cout << "sleep_until woke up on signal: " << sig << "\n";      
+    //   std::cout.flush();
+    // }
+  }  
+  
+  TEST(Improving, WakeupService)
+  {
+    using thread::thread;
+    wakeup_service<signal> wakeup;
+ 
+    for (int i = 0; i < 500; ++i)
+    {
+      thread x (sleep_until, &wakeup, foo);
+      thread y (sleep_until, &wakeup, bar);
+      thread z (sleep_until, &wakeup, baz);
+      x.detach();
+      y.detach();
+      z.detach();
+    }                                      
+    
+    std::chrono::milliseconds d( 1000 );
+    // std::this_thread::yield();
+    
+    std::this_thread::sleep_for(d);
+
+    std::cout << "Beginning wakeup\n";
+    wakeup.wake(foo);
+    wakeup.wake(bar);
+    wakeup.wake(baz);
+    std::cout << "Finishing wakeup\n";
+
+    std::this_thread::sleep_for(d * 2);
+    std::cout << "Number of woken threads: " << count << "\n";
+  }
+  
 }
 
+
+// BOOST_CONCEPT_ASSERT((Moveable<improving<int>>));
+//   // EqualityComparable<improving<T>>
+//   // Monoid<improving<T>>
+// 
+// 
+// void produce(accumulator<int>& x, int interval)
+// {
+//   for (int i; i < 10; ++i)
+//   {
+//     thread::sleep_millis(interval);
+//     x += 1;
+//   }
+//   x.fix();
+// }         
+// 
+// void poll(improving<int>& a, improving<int>& b, improving<int>& c, improving<int>& d, int interval)
+// {
+//   for (int i; i < 10; ++i)
+//   {
+//     thread::sleep_millis(interval);
+//     std::cout << "a: " << a.value() << "\n";
+//     std::cout << "b: " << b.value() << "\n";
+//     std::cout << "c: " << c.value() << "\n";
+//     std::cout << "d: " << d.value() << "\n";
+//     std::cout << "\n";
+//   }
+// }
+// 
+// 
+// TEST(Improving, Basic)
+// {
+//   accumulator<int> x;
+//   accumulator<int> y;
+//   thread::thread(produce(x));
+//   thread::thread(produce(y));
+//   
+//   improving<int> a.get_improving();
+//   improving<int> b.get_improving();
+//   improving<int> c = min(a, b);
+//   improving<int> d = max(a, b);
+// 
+//   produce(x, 10);
+//   produce(y, 11);
+//   poll(a, b, c, d, 100);
+// }
+// 
+//                           
