42c738ea868cae9a39d13211713b9194a63e8425
Hans Höglund <hans@hanshoglund.se>
2012-08-23 18:35:41 +0200

    - Added missing files

---
+++ scl/utility.hpp
@@ -0,0 +1,17 @@
+
+#pragma once
+
+#define SCL_STANDARD_ASSIGN(THIS_TYPE)    \
+  void operator= (const THIS_TYPE& other) \
+  {                                       \
+    THIS_TYPE b = other;                  \
+    return b.swap(*this);                 \
+  }                                       \
+                                          \
+  void operator= (THIS_TYPE&& other)      \
+  {                                       \
+    THIS_TYPE b = std::move(other);       \
+    return b.swap(*this);                 \
+  }                                       \
+
+  
