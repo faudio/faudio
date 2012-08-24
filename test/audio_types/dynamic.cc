a9a60d6078d3384f6aaaec3ec710f1ca9979ded0
Hans Höglund <hans@hanshoglund.se>
2012-08-24 12:19:21 +0200

    - Tests for above

--- test/audio_types/dynamic.cc
+++ test/audio_types/dynamic.cc
@@ -9,9 +9,9 @@ TEST(AudioTypes, Basic)
   // std::shared_ptr<audio_type> a {{ audio_type_tag::sample32 }};
   // std::shared_ptr<audio_type> b {{ audio_type_tag::sample32 }};
 
-  audio_type x = audio_types::sample32();
-  audio_type y = audio_types::pair(audio_types::list(x), audio_types::vector(x));
-  audio_type z = audio_types::pair(x, audio_types::pair(x, x));
+  audio_type x = dynamic::type::sample32();
+  audio_type y = dynamic::type::pair(dynamic::type::list(x), dynamic::type::vector(x));
+  audio_type z = dynamic::type::pair(x, dynamic::type::pair(x, x));
   std::cout << x << "\n";
   std::cout << y << "\n";
   std::cout << z << "\n";
