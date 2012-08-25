7e09ea4777ee63074e88e0acdc6a970c94d63db3
Hans Höglund <hans@hanshoglund.se>
2012-08-26 00:25:08 +0200

    - More tests

--- test/audio_types/alignment.cc
+++ test/audio_types/alignment.cc
@@ -7,6 +7,10 @@
 #include <scl/audio/midi_types.hpp>
 #include <scl/audio/audio_types.hpp>
 
+// using std::pair;
+using std::array;
+using namespace scl::audio;
+
 template <class A, class B>
 struct foo { 
   foo() {}
@@ -45,12 +49,18 @@ void print_offset()
 {
   std::cout << "  offset:    " << offsetof(T,second) << "\n";
 }
+
+struct pair32_64
+{
+  sample32 first;
+  sample64 second;
+};
+
+// #define pair std::pair
+#define pair foo 
   
 TEST(AudioTypes, Alignment)
 {                               
-  using std::pair;
-  using std::array;
-  using namespace scl::audio;
 
   print_name (" midi_simple_message ");
   print_align < midi_simple_message >();
@@ -60,6 +70,13 @@ TEST(AudioTypes, Alignment)
                
   print_name (" sample32 ");
   print_align < sample32 >();
+
+  print_name (" sample64 ");
+  print_align < sample64 >();
+
+  print_name (" pair32_64 ");
+  print_align < pair32_64 >();
+  print_offset< pair32_64 >();
 
   print_name (" pair<sample32,sample32> ");
   print_align < pair<sample32,sample32> >();
@@ -76,11 +93,20 @@ TEST(AudioTypes, Alignment)
   print_name (" array<sample64,10> ");
   print_align < array<sample64,10> >();
 
-  print_name (" pair<array<pair<sample32,sample64>10>,array<sample32,20>> ");
+  print_name (" array<pair32_64,10> ");
+  print_align < array<pair32_64,10> >();
+
+  print_name (" array<pair<sample32,sample64>,10> ");
+  print_align < array<pair<sample32,sample32>,10> >();
+
+  print_name (" array<pair<sample64,sample64>,10> ");
+  print_align < array<pair<sample64,sample32>,10> >();
+
+  print_name (" pair<array<pair<sample32,sample64>,10>,array<sample32,20>> ");
   print_align < pair<array<pair<sample32,sample32>,10>,array<sample32,20>> >();
   print_offset< pair<array<pair<sample32,sample32>,10>,array<sample32,20>> >();
 
-  print_name (" pair<array<pair<sample32,sample32>10>,array<sample32,20>> ");
+  print_name (" pair<array<pair<sample32,sample32>,10>,array<sample32,20>> ");
   print_align < pair<array<pair<sample32,sample32>,10>,array<sample32,20>> >();
   print_offset< pair<array<pair<sample32,sample32>,10>,array<sample32,20>> >();
 
@@ -91,3 +117,77 @@ TEST(AudioTypes, Alignment)
   // std::cout << "std::tuple is trivially copyable: " << 
   //   std::is_trivially_copyable<std::tuple<int,int>>::value << "\n";
 }
+
+
+/*
+   OS X
+    midi_simple_message 
+     size:      3
+     alignment: 1
+    midi_sysex_message 
+     size:      8
+     alignment: 4
+    sample32 
+     size:      4
+     alignment: 4
+    pair<sample32,sample32> 
+     size:      8
+     alignment: 4
+     offset:    4
+    pair<sample32,sample64> 
+     size:      12
+     alignment: 4
+     offset:    4
+    pair<sample64,sample64> 
+     size:      16
+     alignment: 4
+     offset:    8
+    array<sample64,10> 
+     size:      80
+     alignment: 4
+    pair<array<pair<sample32,sample64>10>,array<sample32,20>> 
+     size:      160
+     alignment: 4
+     offset:    80
+    pair<array<pair<sample32,sample32>10>,array<sample32,20>> 
+     size:      160
+     alignment: 4
+     offset:    80
+   
+   Win
+   
+    midi_simple_message                                                                               
+     size:      3                                                                                     
+     alignment: 1                                                                                     
+    midi_sysex_message                                                                                
+     size:      8                                                                                     
+     alignment: 4                                                                                     
+    sample32                                                                                          
+     size:      4                                                                                     
+     alignment: 4                                                                                     
+    pair<sample32,sample32>                                                                           
+     size:      8                                                                                     
+     alignment: 4                                                                                     
+     offset:    4                                                                                     
+    pair<sample32,sample64>                                                                           
+     size:      16                                                                                    
+     alignment: 8                                                                                     
+     offset:    8                                                                                     
+    pair<sample64,sample64>                                                                           
+     size:      16                                                                                    
+     alignment: 8
+     offset:    8
+    array<sample64,10>                                                                                
+     size:      80                                                                                    
+     alignment: 8                                                                                     
+    pair<array<pair<sample32,sample64>10>,array<sample32,20>>                                         
+     size:      160                                                                                   
+     alignment: 4                                                                                     
+     offset:    80                                                                                    
+    pair<array<pair<sample32,sample32>10>,array<sample32,20>>                                         
+     size:      160                                                                                   
+     alignment: 4                                                                                     
+     offset:    80                                                                                    
+    
+   
+ */
