e4fcb629f6e3237513cecc5e217572cd9037066a
Hans Höglund <hans@hanshoglund.se>
2012-12-10 18:41:41 +0100

    - Testing iconv and getopt linkage

--- test/main.c
+++ test/main.c
@@ -4,16 +4,20 @@
 // #include <sndfile.h>
 // #include <portaudio.h>
 // #include <portmidi.h>
+#include <getopt.h>
+#include <iconv.h>
 
 int version[3] = { 2, 0, 0 };
 
 int main (int argc, char const *argv[])
 {
   printf("DoReMIR Audio engine v%d.%d.%d\n", version[0], version[1], version[2]);
-  printf("sizeof(intptr_t) = %d\n", sizeof(intptr_t));
-  printf("sizeof(int32_t) = %d\n", sizeof(int32_t));
-  printf("sizeof(uint32_t) = %d\n", sizeof(uint32_t));
-
+  printf("sizeof(intptr_t) = %d\n", (unsigned int) sizeof(intptr_t));
+  printf("sizeof(int32_t) = %d\n", (unsigned int) sizeof(int32_t));
+  printf("sizeof(uint32_t) = %d\n", (unsigned int) sizeof(uint32_t));
+  
+  int c = getopt(argc, (char**) argv, "abc:");
+  iconv_t cd = iconv_open("WCHAR_T", "UTF-8");
   
   return 0;
 }
