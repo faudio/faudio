81532ff03e7f412b2c7cb47c687a70841b88ac54
Hans Höglund <hans@hanshoglund.se>
2012-12-10 01:25:55 +0100

    - Test impl

--- src/doremir/thread.c
+++ src/doremir/thread.c
@@ -1,34 +1,66 @@
 
-#ifndef _DOREMIR_THREAD
-#define _DOREMIR_THREAD
+struct _doremir_thread_t
+{
+  void * native;
+}
 
-#include <Doremir/Std.h>
+struct _doremir_thread_mutex_t
+{
+  void * native;
+}
 
-/** @defgroup Doremir
-    @{
-    @defgroup Thread
-    @{
-    */
+struct _doremir_thread_condition_t
+{
+  void * native;
+}
 
-typedef struct _doremir_thread_t * doremir_thread_t;
-typedef struct _doremir_thread_mutex_t * doremir_thread_mutex_t;
-typedef struct _doremir_thread_condition_t * doremir_thread_condition_t;
-doremir_thread_t doremir_thread_create_thread();
-void doremir_thread_join(doremir_thread_t);
-void doremir_thread_detach(doremir_thread_t);
-doremir_thread_mutex_t doremir_thread_create_mutex();
-void doremir_thread_destroy_mutex(doremir_thread_mutex_t);
-bool doremir_thread_lock(doremir_thread_mutex_t);
-bool doremir_thread_try_lock(doremir_thread_mutex_t);
-bool doremir_thread_unlock(doremir_thread_mutex_t);
-doremir_thread_condition_t doremir_thread_create_condition();
-void doremir_thread_wait_for(doremir_thread_condition_t);
-void doremir_thread_notify(doremir_thread_condition_t);
-void doremir_thread_notify_all(doremir_thread_condition_t);
+/** Creates a new thread.
+ */
+doremir_thread_t doremir_thread_create_thread()
+{
+}
 
-/** @}
-    @}
-    */
+void doremir_thread_join(doremir_thread_t)
+{
+}
 
-#endif // _DOREMIR_THREAD
+void doremir_thread_detach(doremir_thread_t)
+{
+}
+
+doremir_thread_mutex_t doremir_thread_create_mutex()
+{
+}
+
+void doremir_thread_destroy_mutex(doremir_thread_mutex_t)
+{
+}
+
+bool doremir_thread_lock(doremir_thread_mutex_t)
+{
+}
+
+bool doremir_thread_try_lock(doremir_thread_mutex_t)
+{
+}
+
+bool doremir_thread_unlock(doremir_thread_mutex_t)
+{
+}
+
+doremir_thread_condition_t doremir_thread_create_condition()
+{
+}
+
+void doremir_thread_wait_for(doremir_thread_condition_t)
+{
+}
+
+void doremir_thread_notify(doremir_thread_condition_t)
+{
+}
+
+void doremir_thread_notify_all(doremir_thread_condition_t)
+{
+}
 
