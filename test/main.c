d105c57ee8fb8e05b8b5335cacb8c6bcabedf0a9
Hans Höglund <hans@hanshoglund.se>
2012-12-10 21:36:09 +0100

    - More tests

--- test/main.c
+++ test/main.c
@@ -15,15 +15,22 @@ intptr_t printer(intptr_t x)
     int n = 0;
     while (n < 100)
     {
-        printf("%d\n", n++);
+        printf("%d\n", n);
+        n = n + x;
+        doremir_thread_sleep(100);
     }
     return 0;
 }
 void test_thread()
 {                                               
-    doremir_thread_runnable_t r = { printer, 0 };
+    doremir_thread_runnable_t r = { printer, 10 };
     doremir_thread_t t = doremir_thread_create(r);
+
+    doremir_thread_runnable_t r2 = { printer, 11 };
+    doremir_thread_t t2 = doremir_thread_create(r2);
+
     doremir_thread_join(t);
+    doremir_thread_join(t2);
 }
 
 
