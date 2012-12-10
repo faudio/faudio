6991afcfb1068cd8cd72587583b8b2397a7cc612
Hans Höglund <hans@hanshoglund.se>
2012-12-10 02:18:39 +0100

    - Basic implementations for utility classes

--- src/doremir/atomic.c
+++ src/doremir/atomic.c
@@ -1,29 +1,36 @@
 
-#ifndef _DOREMIR_ATOMIC
-#define _DOREMIR_ATOMIC
+#include <doremir/atomic.h>
 
+struct _doremir_atomic_t
+{
+};
 
+doremir_atomic_t doremir_atomic_create()
+{
+}
 
-/** @defgroup Doremir
-    @{
-    @defgroup Atomic
-    @{
-    */
+doremir_atomic_t doremir_atomic_copy(doremir_atomic_t a)
+{
+}
 
-typedef struct _doremir_atomic_t * doremir_atomic_t;
-typedef intptr_t doremir_atomic_updater_t(intptr_t);
-doremir_atomic_t doremir_atomic_create();
-doremir_atomic_t doremir_atomic_copy(doremir_atomic_t);
-void doremir_atomic_swap(doremir_atomic_t, doremir_atomic_t);
-void doremir_atomic_destroy(doremir_atomic_t);
-intptr_t doremir_atomic_get(doremir_atomic_t);
-void doremir_atomic_set(doremir_atomic_t, intptr_t);
-void doremir_atomic_modify(doremir_atomic_t,
-                           doremir_atomic_updater_t);
+void doremir_atomic_swap(doremir_atomic_t a, doremir_atomic_t b)
+{
+}
 
-/** @}
-    @}
-    */
+void doremir_atomic_destroy(doremir_atomic_t a)
+{
+}
 
-#endif // _DOREMIR_ATOMIC
+intptr_t doremir_atomic_get(doremir_atomic_t a)
+{
+}
+
+void doremir_atomic_set(doremir_atomic_t a, intptr_t v)
+{
+}
+
+void doremir_atomic_modify(doremir_atomic_t a,
+                           doremir_atomic_updater_t f)
+{
+}
 
--- src/doremir/thread.c
+++ src/doremir/thread.c
@@ -1,18 +1,20 @@
 
+#include <doremir/thread.h>
+
 struct _doremir_thread_t
 {
   void * native;
-}
+};
 
 struct _doremir_thread_mutex_t
 {
   void * native;
-}
+};
 
 struct _doremir_thread_condition_t
 {
   void * native;
-}
+};
 
 /** Creates a new thread.
  */
@@ -20,11 +22,11 @@ doremir_thread_t doremir_thread_create_thread()
 {
 }
 
-void doremir_thread_join(doremir_thread_t)
+void doremir_thread_join(doremir_thread_t t)
 {
 }
 
-void doremir_thread_detach(doremir_thread_t)
+void doremir_thread_detach(doremir_thread_t t)
 {
 }
 
@@ -32,19 +34,19 @@ doremir_thread_mutex_t doremir_thread_create_mutex()
 {
 }
 
-void doremir_thread_destroy_mutex(doremir_thread_mutex_t)
+void doremir_thread_destroy_mutex(doremir_thread_mutex_t t)
 {
 }
 
-bool doremir_thread_lock(doremir_thread_mutex_t)
+bool doremir_thread_lock(doremir_thread_mutex_t t)
 {
 }
 
-bool doremir_thread_try_lock(doremir_thread_mutex_t)
+bool doremir_thread_try_lock(doremir_thread_mutex_t t)
 {
 }
 
-bool doremir_thread_unlock(doremir_thread_mutex_t)
+bool doremir_thread_unlock(doremir_thread_mutex_t t)
 {
 }
 
@@ -52,15 +54,15 @@ doremir_thread_condition_t doremir_thread_create_condition()
 {
 }
 
-void doremir_thread_wait_for(doremir_thread_condition_t)
+void doremir_thread_wait_for(doremir_thread_condition_t c)
 {
 }
 
-void doremir_thread_notify(doremir_thread_condition_t)
+void doremir_thread_notify(doremir_thread_condition_t c)
 {
 }
 
-void doremir_thread_notify_all(doremir_thread_condition_t)
+void doremir_thread_notify_all(doremir_thread_condition_t c)
 {
 }
 
