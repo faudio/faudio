7bf26c5c77aa04be84089df16d733801639309a0
Hans Höglund <hans@hanshoglund.se>
2012-12-10 02:59:20 +0100

    - Thread and atomic

--- src/doremir/atomic.c
+++ src/doremir/atomic.c
@@ -3,14 +3,18 @@
 
 struct _doremir_atomic_t
 {
+    intptr_t value;
 };
 
 doremir_atomic_t doremir_atomic_create()
 {
+    doremir_atomic_t a = malloc(sizeof(struct _doremir_atomic_t));
+    return a;
 }
 
 doremir_atomic_t doremir_atomic_copy(doremir_atomic_t a)
 {
+    free(a);
 }
 
 void doremir_atomic_swap(doremir_atomic_t a, doremir_atomic_t b)
@@ -34,3 +38,104 @@ void doremir_atomic_modify(doremir_atomic_t a,
 {
 }
 
+
+
+
+/*
+  #ifdef SCL_ATOMIC_OSX
+      template <>
+      struct default_atomic_traits<int32_t>
+      {
+          inline static bool swap(int32_t* a, int32_t x, int32_t y)
+          {
+              return OSAtomicCompareAndSwap32Barrier(x, y, a);
+          }
+          static int32_t get(int32_t* a)
+          {
+              return OSAtomicAdd32Barrier(0, a);
+          }
+          static void add(int32_t* a, int32_t x)
+          {
+              OSAtomicAdd32Barrier(x, a);
+          }
+      };
+
+      template <class T>
+      struct default_atomic_traits<T*>
+      {
+          static bool swap(T** a, T* x, T* y)
+          {
+              return OSAtomicCompareAndSwapPtrBarrier((void*) x, (void*) y, (void**) a);
+          }
+          static T* get(T** a)
+          {
+              return *a;
+          }
+      };
+  #endif
+
+
+  #ifdef SCL_ATOMIC_WIN
+      template <>
+      struct default_atomic_traits<int32_t>
+      {
+          static bool swap(int32_t* a, int32_t x, int32_t y)
+          {
+              return ( x == InterlockedCompareExchange((LONG*) a, (LONG) y, (LONG) x) );
+          }
+          static int32_t get(int32_t* a)
+          {
+              return InterlockedCompareExchange((LONG*) a, (LONG) 0, (LONG) 0);
+          }
+          static void add(int32_t* a, int32_t x)
+          {
+              InterlockedExchangeAdd((LONG*) a, (LONG) x);
+          }
+      };
+
+      template <class T>
+      struct default_atomic_traits<T*>
+      {
+          static bool swap(T** a, T* x, T* y)
+          {
+              return ( x == InterlockedCompareExchangePointer((void**) a, (void*) y, (void*) x) );
+          }
+          static T* get(T** a)
+          {
+              return reinterpret_cast<T*>(InterlockedCompareExchangePointer((void**) a, (void*) 0, (void*) 0));
+          }
+      };
+  #endif
+
+  #ifdef SCL_ATOMIC_GCC
+      template <>
+      struct default_atomic_traits<int32_t>
+      {
+          static bool swap(int32_t* a, int32_t x, int32_t y)
+          {
+              return __sync_bool_compare_and_swap(a, x, y);
+          }
+          static int32_t get(int32_t* a)
+          {
+              return __sync_val_compare_and_swap(a, 0, 0);
+          }
+          static void add(int32_t* a, int32_t x)
+          {
+              __sync_fetch_and_add (a, x);
+          }
+      };
+
+      template <class T>
+      struct default_atomic_traits<T*>
+      {
+          static bool swap(T** a, T* x, T* y)
+          {
+              return __sync_bool_compare_and_swap(a, x, y);
+          }
+          static T* get(T** a)
+          {
+              return __sync_val_compare_and_swap(a, 0, 0);
+          }
+      };
+  #endif
+*/
--- src/doremir/thread.c
+++ src/doremir/thread.c
@@ -4,105 +4,112 @@
 
 struct _doremir_thread_t
 {
-  pthread_t native;
+    pthread_t native;
 };
 
 struct _doremir_thread_mutex_t
 {
-  pthread_mutex_t native;
+    pthread_mutex_t native;
 };
 
 struct _doremir_thread_condition_t
 {
-  pthread_cond_t native;
+    pthread_cond_t native;
 };
 
 /** Creates a new thread.
  */
 doremir_thread_t doremir_thread_create_thread()
 {
-  doremir_thread_t t = malloc(sizeof(struct _doremir_thread_t));
-  // TODO pass func
-  int r = pthread_create(&t->native, NULL, NULL, NULL);
-  if (r != 0)
-    ; // TODO fatal
+    doremir_thread_t t = malloc(sizeof(struct _doremir_thread_t));
+    // TODO pass func
+    int r = pthread_create(&t->native, NULL, NULL, NULL);
+    if (r != 0)
+        ; // TODO fatal
+    return t;
 }
 
 void doremir_thread_join(doremir_thread_t t)
 {
-  int r = pthread_join(t->native, NULL);
-  if (r != 0)
-    ; // TODO fatal
+    int r = pthread_join(t->native, NULL);
+    free(t);
+    if (r != 0)
+        ; // TODO fatal
 }
 
 void doremir_thread_detach(doremir_thread_t t)
 {
-  int r = pthread_detach(t->native);
-  if (r != 0)
-    ; // TODO fatal
+    int r = pthread_detach(t->native);
+    free(t);
+    if (r != 0)
+        ; // TODO fatal
 }
 
 doremir_thread_mutex_t doremir_thread_create_mutex()
 {
-  doremir_thread_mutex_t m = malloc(sizeof(struct _doremir_thread_mutex_t));
-  int r = pthread_mutex_init(&m->native, NULL);
-  if (r != 0)
-    ; // TODO fatal
+    doremir_thread_mutex_t m = malloc(sizeof(struct _doremir_thread_mutex_t));
+    int r = pthread_mutex_init(&m->native, NULL);
+    if (r != 0)
+        ; // TODO fatal
+    return m;
 }
 
-void doremir_thread_destroy_mutex(doremir_thread_mutex_t t)
-{           
-  int r = pthread_mutex_destroy(&t->native);
-  if (r != 0)
-    ; // TODO fatal
+void doremir_thread_destroy_mutex(doremir_thread_mutex_t m)
+{                     
+    int r = pthread_mutex_destroy(&m->native);
+    free(m);
+    if (r != 0)
+        ; // TODO fatal
 }
 
 bool doremir_thread_lock(doremir_thread_mutex_t t)
 { 
-  int r = 0;
-  if (r != 0)
-    ; // TODO fatal
+    int r = 0;
+    if (r != 0)
+        ; // TODO fatal
 }
 
 bool doremir_thread_try_lock(doremir_thread_mutex_t t)
 {
-  int r = 0;
-  if (r != 0)
-    ; // TODO fatal
+    int r = 0;
+    if (r != 0)
+        ; // TODO fatal
 }
 
 bool doremir_thread_unlock(doremir_thread_mutex_t t)
 {
-  int r = 0;
-  if (r != 0)
-    ; // TODO fatal
+    int r = 0;
+    if (r != 0)
+        ; // TODO fatal
 }
 
 doremir_thread_condition_t doremir_thread_create_condition()
 {
-  int r = 0;
-  if (r != 0)
-    ; // TODO fatal
+    int r = 0;
+    if (r != 0)
+        ; // TODO fatal
+    // TODO return c
 }
 
 void doremir_thread_wait_for(doremir_thread_condition_t c)
 {
-  int r = 0;
-  if (r != 0)
-    ; // TODO fatal
+    int r = 0;
+    // TODO free
+    if (r != 0)
+        ; // TODO fatal
 }
 
 void doremir_thread_notify(doremir_thread_condition_t c)
 {
-  int r = 0;
-  if (r != 0)
-    ; // TODO fatal
+    int r = 0;
+    if (r != 0)
+        ; // TODO fatal
 }
 
 void doremir_thread_notify_all(doremir_thread_condition_t c)
 {
-  int r = 0;
-  if (r != 0)
-    ; // TODO fatal
+    int r = 0;
+    if (r != 0)
+        ; // TODO fatal
 }
 
