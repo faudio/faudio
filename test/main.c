dcc726af601b8fbb54b094982cb1dd63c50cd5d1
Hans Höglund <hans@hanshoglund.se>
2012-12-10 02:31:12 +0100

    - Test tests

--- test/main.c
+++ test/main.c
@@ -1,8 +1,14 @@
 
 #include <doremir/audio_engine.h>
 
+int version[3] = { 2, 0, 0 };
+
 int main (int argc, char const *argv[])
 {
-  /* code */
+  printf("DoReMIR Audio engine v%d.%d.%d\n", version[0], version[1], version[2]);
+
+
+
+
   return 0;
 }
