04652ec696f2bbc82ab1a89a073900a0a84f4c72
Hans Höglund <hans@hanshoglund.se>
2012-12-10 20:38:22 +0100

    - Copied thread impl to Win

--- src/doremir/thread_win.c
+++ src/doremir/thread_win.c
@@ -14,26 +14,47 @@ struct _doremir_thread_condition_t
 {
 };
 
-/** Creates a new thread.
- */
-doremir_thread_t doremir_thread_create_thread()
+doremir_thread_t doremir_thread_create()
 {
+    doremir_thread_t t = malloc(sizeof(struct _doremir_thread_t));
+    // TODO
+    int r = 0;
+    if (r != 0)
+        ; // TODO fatal
+    return t;
 }
 
 void doremir_thread_join(doremir_thread_t t)
 {
+    int r = pthread_join(t->native, NULL);
+    free(t);
+    if (r != 0)
+        ; // TODO fatal
 }
 
 void doremir_thread_detach(doremir_thread_t t)
 {
+    int r = pthread_detach(t->native);
+    free(t);
+    if (r != 0)
+        ; // TODO fatal
 }
 
 doremir_thread_mutex_t doremir_thread_create_mutex()
 {
+    doremir_thread_mutex_t m = malloc(sizeof(struct _doremir_thread_mutex_t));
+    int r = pthread_mutex_init(&m->native, NULL);
+    if (r != 0)
+        ; // TODO fatal
+    return m;
 }
 
 void doremir_thread_destroy_mutex(doremir_thread_mutex_t m)
 {                     
+    int r = pthread_mutex_destroy(&m->native);
+    free(m);
+    if (r != 0)
+        ; // TODO fatal
 }
 
 bool doremir_thread_lock(doremir_thread_mutex_t t)
@@ -65,6 +86,14 @@ doremir_thread_condition_t doremir_thread_create_condition()
     // TODO return c
 }
 
+void doremir_thread_destroy_condition(doremir_thread_condition_t c)
+{
+    int r = 0;
+    if (r != 0)
+        ; // TODO fatal
+    // TODO return c    
+}
+
 void doremir_thread_wait_for(doremir_thread_condition_t c)
 {
     int r = 0;
@@ -86,4 +115,3 @@ void doremir_thread_notify_all(doremir_thread_condition_t c)
     if (r != 0)
         ; // TODO fatal
 }
-
