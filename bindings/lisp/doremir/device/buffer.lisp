756675655887da147be4cdc47591ea157f2a4191
Hans Höglund <hans@hanshoglund.se>
2012-12-12 15:56:40 +0100

    - Cleaning

--- bindings/lisp/doremir/device/buffer.lisp
+++ bindings/lisp/doremir/device/buffer.lisp
@@ -1,5 +1,7 @@
 (defctype Doremir.Device.Buffer :pointer)
 
+(defctype Doremir.Device.Buffer.Stream :pointer)
+
 (defcfun "Doremir.Device.Buffer.create" :Doremir.Device.Buffer (:size))
 
 (defcfun "Doremir.Device.Buffer.destroy" :void (:Doremir.Device.Buffer))
