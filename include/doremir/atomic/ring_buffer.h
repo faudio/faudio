80d72d689227c51b3b49ec784ec2582731852224
Hans Höglund <hans@hanshoglund.se>
2012-12-12 21:17:00 +0100

    - RingBuffer interface

--- include/doremir/atomic/ring_buffer.h
+++ include/doremir/atomic/ring_buffer.h
@@ -13,11 +13,14 @@
     */
 
 typedef struct _doremir_atomic_ring_buffer_t * doremir_atomic_ring_buffer_t;
-doremir_atomic_ring_buffer_t doremir_atomic_ring_buffer_create(doremir_atomic_ring_buffer_t);
+doremir_atomic_ring_buffer_t doremir_atomic_ring_buffer_create(size_t);
 doremir_atomic_ring_buffer_t doremir_atomic_ring_buffer_copy(doremir_atomic_ring_buffer_t);
+doremir_atomic_ring_buffer_t doremir_atomic_ring_buffer_copy_sized(size_t,
+                                                                   doremir_atomic_ring_buffer_t);
 void doremir_atomic_ring_buffer_swap(doremir_atomic_ring_buffer_t,
                                      doremir_atomic_ring_buffer_t);
 void doremir_atomic_ring_buffer_destroy(doremir_atomic_ring_buffer_t);
+size_t doremir_atomic_ring_buffer_size(doremir_atomic_ring_buffer_t);
 uint8_t doremir_atomic_ring_buffer_read(doremir_atomic_ring_buffer_t);
 void doremir_atomic_ring_buffer_write(doremir_atomic_ring_buffer_t,
                                       uint8_t);
