74dd8ed057b92997313296d596802dcfd2c17978
Hans Höglund <hans@hanshoglund.se>
2012-12-16 15:48:05 +0100

    - Interface IDs

--- include/doremir/interfaces.h
+++ include/doremir/interfaces.h
@@ -2,11 +2,19 @@
 #ifndef _DOREMIR_INTERFACES
 #define _DOREMIR_INTERFACES
 
+/* 
+    Ideally, Modulo would generate interface identifiers for us.
+    Until that day we declare all in this enum to keep them unique.
+ */
 enum doremir_interfaces {
     doremir_equal_i,
+    doremir_order_i,
     doremir_copy_i,
     doremir_destroy_i,
-    doremir_closure_i
+    doremir_dynamic,
+    doremir_scheduler_i,
+    doremir_processor_i,
+    doremir_number_i
 };
 
 #endif // _DOREMIR_INTERFACES
