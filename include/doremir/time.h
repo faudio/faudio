cba4bcc28aa85ca3b54755008de61f9226dcb456
Hans Höglund <hans@hanshoglund.se>
2012-12-17 18:32:44 +0100

    - Added time types

--- include/doremir/time.h
+++ include/doremir/time.h
@@ -2,7 +2,8 @@
 #ifndef _DOREMIR_TIME
 #define _DOREMIR_TIME
 
-
+#include <doremir.h>
+#include <doremir/ratio.h>
 
 /** @defgroup Doremir Doremir
     @{
@@ -10,7 +11,12 @@
     @{
     */
 
-
+typedef enum {
+            second, minute, hour, day
+        } doremir_time_unit_t;
+typedef struct {
+            doremir_ratio_t value; doremir_time_unit_t unit;
+        } doremir_time_t;
 
 /** @}
     @}
