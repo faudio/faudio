4b0510c9aae8981e74bf6dd6fce5718354e10b45
Hans Höglund <hans@hanshoglund.se>
2013-01-03 19:31:21 +0100

    - Cleaning

--- include/doremir/string.h
+++ include/doremir/string.h
@@ -10,11 +10,11 @@
     @{
     */
 
-typedef struct _doremir_string_t * doremir_string_t;
-typedef doremir_string_t doremir_string_file_path_t;
 typedef doremir_char8_t * doremir_string_utf8_t;
 typedef doremir_char16_t * doremir_string_utf16_t;
 typedef doremir_char32_t * doremir_string_utf32_t;
+typedef struct _doremir_string_t * doremir_string_t;
+typedef doremir_string_t doremir_string_file_path_t;
 doremir_string_t doremir_string_empty();
 doremir_string_t doremir_string_single(doremir_char16_t);
 doremir_string_t doremir_string_copy(doremir_string_t);
@@ -23,19 +23,19 @@ doremir_string_t doremir_string_append(doremir_string_t,
 doremir_string_t doremir_string_dappend(doremir_string_t,
                                         doremir_string_t);
 void doremir_string_destroy(doremir_string_t);
-doremir_string_t doremir_string_format_integer(char *, long);
 int doremir_string_length(doremir_string_t);
 doremir_char16_t doremir_string_char_at(int, doremir_string_t);
-typedef struct {
-            doremir_string_t (* show)(doremir_ptr_t);
-        } doremir_string_show_t;
-doremir_string_t doremir_string_show(doremir_ptr_t);
 doremir_string_utf8_t doremir_string_to_utf8(doremir_string_t);
 doremir_string_utf16_t doremir_string_to_utf16(doremir_string_t);
 doremir_string_utf32_t doremir_string_to_utf32(doremir_string_t);
 doremir_string_t doremir_string_from_utf8(doremir_string_utf8_t);
 doremir_string_t doremir_string_from_utf16(doremir_string_utf16_t);
 doremir_string_t doremir_string_from_utf32(doremir_string_utf32_t);
+doremir_string_t doremir_string_format_integer(char *, long);
+typedef struct {
+            doremir_string_t (* show)(doremir_ptr_t);
+        } doremir_string_show_t;
+doremir_string_t doremir_string_show(doremir_ptr_t);
 
 /** @}
     @}
