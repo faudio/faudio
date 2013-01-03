4a51e4691d0bfb03d9609fb057baedec79207a3a
Hans Höglund <hans@hanshoglund.se>
2013-01-04 00:56:02 +0100

    - Basic ringbuffer impl

--- include/doremir/atomic/ring_buffer.h
+++ include/doremir/atomic/ring_buffer.h
@@ -22,7 +22,7 @@ void doremir_atomic_ring_buffer_swap(doremir_atomic_ring_buffer_t,
 void doremir_atomic_ring_buffer_destroy(doremir_atomic_ring_buffer_t);
 size_t doremir_atomic_ring_buffer_size(doremir_atomic_ring_buffer_t);
 uint8_t doremir_atomic_ring_buffer_read(doremir_atomic_ring_buffer_t);
-void doremir_atomic_ring_buffer_write(doremir_atomic_ring_buffer_t,
+bool doremir_atomic_ring_buffer_write(doremir_atomic_ring_buffer_t,
                                       uint8_t);
 
 /** @}
