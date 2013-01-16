36a817179f5aa01a7202c17b3f3944b28f6aea07
Hans Höglund <hans@hanshoglund.se>
2013-01-16 18:43:01 +0100

    - Removed old bindings file

--- bindings/lisp/doremir/dispatcher.lisp
+++
@@ -1,9 +0,0 @@
-(defctype Doremir.Dispatcher.Message :Doremir.List)
-
-(defctype Doremir.Dispatcher.Address :Doremir.Ptr)
-
-(defctype Doremir.Dispatcher.Sender (:pointer :void))
-
-(defctype Doremir.Dispatcher.Receiver (:pointer :void))
-
-(defctype Doremir.Dispatcher (:pointer :void))
