89fdaa85c7ebcb374429a1c5b8bc5ca9c0ef9cb7
Hans Höglund <hans@hanshoglund.se>
2013-01-23 23:42:49 +0100

    - Added size_min

--- include/doremir/util/minmax.h
+++ include/doremir/util/minmax.h
@@ -3,6 +3,7 @@
 #define _DOREMIR_UTIL_MINMAX
 
 inline static size_t size_max(size_t a, size_t b) { return (a < b) ? a : b; }
+inline static size_t size_min(size_t a, size_t b) { return (a < b) ? b : a; }
 
 #endif // _DOREMIR_UTIL_MINMAX
 
