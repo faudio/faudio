82c3a1f3a6dca16abbb3b3ff401c217986fa1918
Hans Höglund <hans@hanshoglund.se>
2013-01-25 02:28:38 +0100

    - Added Graph to Makefile (missing)

---
+++ include/doremir/graph.h
@@ -0,0 +1,42 @@
+
+#ifndef _DOREMIR_GRAPH
+#define _DOREMIR_GRAPH
+
+#include <doremir.h>
+#include <doremir/pair.h>
+#include <doremir/list.h>
+#include <doremir/string.h>
+
+/** @defgroup Doremir Doremir
+    @{
+    @defgroup DoremirGraph Graph
+    @{
+    */
+
+typedef struct _doremir_graph_t * doremir_graph_t;
+typedef doremir_ptr_t doremir_graph_node_t;
+typedef doremir_ptr_t doremir_graph_edge_t;
+typedef doremir_list_t doremir_graph_node_list_t;
+typedef doremir_list_t doremir_graph_edge_list_t;
+doremir_graph_t doremir_graph_empty();
+doremir_graph_t doremir_graph_insert(doremir_graph_node_t,
+                                     doremir_graph_t);
+doremir_graph_t doremir_graph_remove(doremir_graph_node_t,
+                                     doremir_graph_t);
+void doremir_graph_destroy(doremir_graph_t);
+doremir_graph_t doremir_graph_connect(doremir_graph_edge_t,
+                                      doremir_graph_node_t,
+                                      doremir_graph_node_t,
+                                      doremir_graph_t);
+doremir_graph_t doremir_graph_disconnect(doremir_graph_node_t,
+                                         doremir_graph_node_t,
+                                         doremir_graph_t);
+doremir_graph_t doremir_graph_from_list(doremir_pair_t);
+doremir_list_t doremir_graph_to_list(doremir_graph_t);
+
+/** @}
+    @}
+    */
+
+#endif // _DOREMIR_GRAPH
+
