0fef3452ef9a01e8a5d6d9b6b854996190cc05b9
Hans Höglund <hans@hanshoglund.se>
2013-02-04 00:22:09 +0100

    - File device impl

--- src/doremir/device/file.c
+++ src/doremir/device/file.c
@@ -11,13 +11,28 @@
 #include <doremir/util.h>
 #include <sndfile.h>
 
+typedef doremir_device_file_t                  device_t;
+typedef doremir_device_file_result_t           result_t;
+
 struct _doremir_device_file_t {
-    impl_t      impl;
-    string_t    path;
+    impl_t              impl;
+    string_t            path;
+    SNDFILE      *      file;
+};
+
+struct _doremir_device_file_result_t {
+
+    device_t            input, output;
+    processor_t         proc;
+    proc_interface_t  * proc_impl;
+    int32_t             time;
+    dispatcher_t        in_disp;
+    dispatcher_t        out_disp;
+
 };
 
 file_device_t doremir_device_file_open(string_t path)
-{       
+{
     inform(string_dappend(string("Opening file stream "), doremir_string_copy(path)));
 
     {
@@ -33,9 +48,9 @@ void doremir_device_file_close(file_device_t device)
 }
 
 doremir_device_file_result_t doremir_device_file_run(
-        file_device_t input,
-        processor_t   processor,
-        file_device_t output)
+    file_device_t input,
+    processor_t   processor,
+    file_device_t output)
 {
 }
 
