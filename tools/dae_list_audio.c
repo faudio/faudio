8ec7b97b02c460fb2637bcd7c5da23523d924b32
Hans Höglund <hans@hanshoglund.se>
2013-02-08 22:04:56 +0100

    - Basic tools

---
+++ tools/dae_list_audio.c
@@ -0,0 +1,44 @@
+
+#include <doremir/audio_engine.h>
+#include <doremir/util.h>
+
+void print_audio_devices(audio_session_t session)
+{
+    doremir_print("Audio devices: \n", NULL);
+    doremir_for_each(x, doremir_device_audio_all(session)) {
+        doremir_print("    Device: %s\n", x);
+        doremir_print("        Input:  %s\n", doremir_device_audio_input_type(x));
+        doremir_print("        Output: %s\n", doremir_device_audio_output_type(x));
+    }
+    doremir_print("Default input is : %s\n", doremir_device_audio_default_input(session));
+    doremir_print("Default output is : %s\n", doremir_device_audio_default_output(session));
+    doremir_print("\n", NULL);
+}
+
+int main (int argc, char const *argv[])
+{
+    audio_session_t session;
+    
+    doremir_audio_engine_initialize();
+    
+    // Begin session
+    session = doremir_device_audio_begin_session();
+
+    // Handle possible error
+    if (doremir_check(session)) {
+        log_error((error_t) session);
+        warn(string("Aborting test due to error"));
+        goto error;
+    }
+
+    // Session obtained, we can now access devices
+    print_audio_devices(session);
+
+    doremir_device_audio_end_session(session);
+    doremir_audio_engine_terminate();
+    return 0;
+error:
+    doremir_device_audio_end_session(session);
+    doremir_audio_engine_terminate();
+    return -1;
+}
---
+++ tools/dae_list_midi.c
@@ -0,0 +1,44 @@
+
+#include <doremir/audio_engine.h>
+#include <doremir/util.h>
+
+void print_midi_devices(midi_session_t session)
+{
+    doremir_print("Listing midi devices: \n", NULL);
+    doremir_for_each(x, doremir_device_midi_all(session)) {
+        doremir_print("    Device: %s\n", x);
+        doremir_print("        Input:  %s\n", b(doremir_device_midi_has_input(x)));
+        doremir_print("        Output: %s\n", b(doremir_device_midi_has_output(x)));
+    }
+    doremir_print("Default input is : %s\n", doremir_device_midi_default_input(session));
+    doremir_print("Default output is : %s\n", doremir_device_midi_default_output(session));
+    doremir_print("\n", NULL);
+}
+
+int main (int argc, char const *argv[])
+{
+    midi_session_t session;
+    
+    doremir_audio_engine_initialize();
+    
+    // Begin session
+    session = doremir_device_midi_begin_session();
+
+    // Handle possible error
+    if (doremir_check(session)) {
+        log_error((error_t) session);
+        warn(string("Aborting test due to error"));
+        goto error;
+    }
+
+    // Session obtained, we can now access devices
+    print_midi_devices(session);
+
+    doremir_device_midi_end_session(session);
+    doremir_audio_engine_terminate();
+    return 0;
+error:
+    doremir_device_midi_end_session(session);
+    doremir_audio_engine_terminate();
+    return -1;
+}
