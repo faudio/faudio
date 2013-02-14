b9393e9d2ca500a154b44c85647d2884322f889f
Hans Höglund <hans@hanshoglund.se>
2013-02-14 23:49:35 +0100

    Cleaned Event module interface

--- include/doremir/event.h
+++ include/doremir/event.h
@@ -36,7 +36,6 @@ doremir_event_t doremir_event_after(doremir_event_t,
 doremir_event_t doremir_event_before(doremir_event_t,
                                      doremir_event_t);
 doremir_time_t doremir_event_offset(doremir_event_t);
-void doremir_event_sync(doremir_event_t);
 void doremir_event_add_sync(void (*)(doremir_ptr_t,
                                      doremir_message_sender_t),
                             doremir_ptr_t,
