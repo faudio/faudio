8a6d5ab9bd7b9a92c354a0289cc4cd0c12d3bbd0
Hans Höglund <hans@hanshoglund.se>
2013-02-18 01:50:31 +0100

    Improved device detection in ae_echo_midi

--- tools/ae_echo_midi.c
+++ tools/ae_echo_midi.c
@@ -2,6 +2,25 @@
 #include <doremir/audio_engine.h>
 #include <doremir/util.h>
 
+midi_device_t find_device(midi_session_t session, string_t pattern) {
+    doremir_for_each(device, doremir_device_midi_all(session)) {
+        if (doremir_string_matches(pattern,
+                                   doremir_device_midi_name(device)))
+            return device;
+    }
+    return NULL;
+}
+
+midi_device_t find_input(midi_session_t session, string_t pattern) {
+    midi_device_t device = find_device(session, pattern);
+    return device ? device : doremir_device_midi_default_input(session);
+}
+
+midi_device_t find_output(midi_session_t session, string_t pattern) {
+    midi_device_t device = find_device(session, pattern);
+    return device ? device : doremir_device_midi_default_output(session);
+}
+
 void echo()
 {
     midi_session_t  session;
@@ -18,11 +37,10 @@ void echo()
         goto cleanup;
     }
 
-    input = doremir_list_index(2, doremir_device_midi_all(session));
-    output = doremir_list_index(6, doremir_device_midi_all(session));
-
-    in_stream  = doremir_device_midi_open_stream(input);
-    out_stream = doremir_device_midi_open_stream(output);
+    input       = find_input(session, string(""));
+    output      = find_output(session, string(""));
+    in_stream   = doremir_device_midi_open_stream(input);
+    out_stream  = doremir_device_midi_open_stream(output);
 
     if (doremir_check(in_stream)) {
         log_error((error_t) in_stream);
