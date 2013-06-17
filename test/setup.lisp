a24dbe896cbea49e3750ade5d5ee0d50632dc8ef
Hans Höglund <hans@hanshoglund.se>
2013-06-17 12:17:50 +0200

    Updated Lisp test paths
    See #1

--- test/setup.lisp
+++ test/setup.lisp
@@ -13,9 +13,9 @@
 (asdf:load-system :audio-engine)
 
 ; Load library and setup tests
-(let ((framework-name "DoReMIRAudio")
+(let ((framework-name "FAE")
       (framework-path (format nil "~a/audio/build/Frameworks/" (user-homedir-pathname)))
-      (log-path       (format nil "~a/Library/Logs/DoReMIRAudio.log" (user-homedir-pathname))))
+      (log-path       (format nil "~a/Library/Logs/FAE.log" (user-homedir-pathname))))
   (push framework-path cffi:*darwin-framework-directories*)
   (setf *foreign-lib* (cffi:load-foreign-library `(:framework ,framework-name)))
   (audio-engine::audioengine-set-log-file log-path)
