40a30d77520205b57fea718a7333c99945ac3247
Hans Höglund <hans@hanshoglund.se>
2013-06-25 00:09:29 +0200

    Cleans examples

--- examples/fae_list_audio.c
+++ examples/fae_list_audio.c
@@ -2,40 +2,39 @@
 #include <fae/fae.h>
 #include <fae/util.h>
 
-void print_audio_devices(audio_session_t session)
+/*
+    This program prints the currently connected MIDI devices.
+    
+    The output consists of device declarations separated by blank lines.
+
+    An example device declaration:
+
+        Name:           Built-in Microphone
+        Host:           Core Audio
+        Input:          2
+        Output:         0     
+        
+    The actual output has no indentation.
+ */
+
+fae_audio_session_t print_audio_devices(fae_ptr_t _, audio_session_t session)
 {
-    fae_print("Audio devices: \n", NULL);
     fae_for_each(x, fae_audio_all(session)) {
-        fae_print("    Device: %s\n", x);
-        fae_print("        Input:  %s\n", fae_audio_input_type(x));
-        fae_print("        Output: %s\n", fae_audio_output_type(x));
+        fae_print("Name:           %s\n", fae_string_to_string(fae_audio_name(x)));
+        fae_print("Host:           %s\n", fae_string_to_string(fae_audio_host_name(x)));
+        fae_print("Input:          %s\n", i16(fae_type_channels(fae_audio_input_type(x))));
+        fae_print("Output:         %s\n", i16(fae_type_channels(fae_audio_output_type(x))));
+        fae_print_ln(string(""));
     }
-    fae_print("Default input is : %s\n", fae_audio_default_input(session));
-    fae_print("Default output is : %s\n", fae_audio_default_output(session));
-    fae_print("\n", NULL);
+    return session;
 }
 
 int main (int argc, char const *argv[])
-{    
-    audio_session_t session;    
-    
+{
     fae_fae_initialize();
-    session = fae_audio_begin_session();
-
-    if (fae_check(session)) {
-        log_error((error_t) session);
-        goto error;
-    }
-
-    print_audio_devices(session);
-
-    fae_audio_end_session(session);
+    fae_audio_with_session(
+        print_audio_devices, NULL, 
+        fae_error_log, NULL);
     fae_fae_terminate();
-
-    return 0;
-
-error:
-    fae_audio_end_session(session);
-    fae_fae_terminate();
-    return -1;
 }
+
--- examples/fae_list_midi.c
+++ examples/fae_list_midi.c
@@ -2,51 +2,38 @@
 #include <fae/fae.h>
 #include <fae/util.h>
 
-void print_midi_devices(midi_session_t session)
+/*
+    This program prints the currently connected MIDI devices.
+    
+    The output consists of device declarations separated by blank lines.
+
+    An example device declaration:
+
+        Name:           Network Session 1
+        Host:           CoreMIDI
+        Input:          true
+        Output:         false     
+        
+    The actual output has no indentation.
+ */
+
+fae_midi_session_t print_midi_devices(fae_ptr_t _, midi_session_t session)
 {
-    fae_print("Listing midi devices: \n", NULL);                           
-
     fae_for_each(x, fae_midi_all(session)) {
+        fae_print("Name:           %s\n", fae_string_to_string(fae_midi_name(x)));
+        fae_print("Host:           %s\n", fae_string_to_string(fae_midi_host_name(x)));
+        fae_print("Input:          %s\n", fb(fae_midi_has_input(x)));
+        fae_print("Output:         %s\n", fb(fae_midi_has_output(x)));
         fae_print_ln(string(""));
-        fae_print("    Device:        %s\n", fae_string_to_string(fae_midi_name(x)));
-        fae_print("    Host:          %s\n", fae_string_to_string(fae_midi_host_name(x)));
-        fae_print("    Input:         %s\n", fb(fae_midi_has_input(x)));
-        fae_print("    Output:        %s\n", fb(fae_midi_has_output(x)));
     }
-
-    fae_print_ln(string(""));
-
-    fae_print("Default input is:  %s\n", fae_string_to_string(
-        fae_midi_name(
-            fae_midi_default_input(session))));
-
-    fae_print("Default output is: %s\n", fae_string_to_string(
-        fae_midi_name(
-            fae_midi_default_output(session))));
-
-    fae_print_ln(string(""));
+    return session;
 }
 
 int main (int argc, char const *argv[])
 {
-    midi_session_t session;
-    
     fae_fae_initialize();
-    
-    session = fae_midi_begin_session();
-
-    if (fae_check(session)) {
-        log_error((error_t) session);
-        goto error;
-    }
-
-    print_midi_devices(session);
-
-    fae_midi_end_session(session);
+    fae_midi_with_session(
+        print_midi_devices, NULL, 
+        fae_error_log, NULL);
     fae_fae_terminate();
-    return 0;
-error:
-    fae_midi_end_session(session);
-    fae_fae_terminate();
-    return -1;
 }
