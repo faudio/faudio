53dd9a94320d1f2c211b39721d70008686a21521
Hans Höglund <hans@hanshoglund.se>
2013-09-21 00:09:24 +0200

    Cleaning

--- include/fa/interfaces.h
+++ include/fa/interfaces.h
@@ -7,20 +7,20 @@
     Until that day we declare all in this enum to keep them unique.
  */
 enum fa_interfaces {
-  fa_copy_i,
-  fa_destroy_i,
-  fa_dynamic_i,
-  fa_error_i,
-  fa_equal_i,
+    fa_copy_i,
+    fa_destroy_i,
+    fa_dynamic_i,
+    fa_error_i,
+    fa_equal_i,
 
-  fa_time_clock_interface_i,
-  fa_message_sender_interface_i,
-  fa_message_receiver_interface_i,
-  fa_number_i,
-  fa_order_i,
+    fa_time_clock_interface_i,
+    fa_message_sender_interface_i,
+    fa_message_receiver_interface_i,
+    fa_number_i,
+    fa_order_i,
 
-  fa_processor_interface_i,
-  fa_string_show_i
+    fa_processor_interface_i,
+    fa_string_show_i
 };
 
 #endif // _FA_INTERFACES
