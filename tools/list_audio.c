6c8b238eceeacb4aed2014e5ce595d8ff506e8e5
Hans Höglund <hans@hanshoglund.se>
2013-09-22 01:35:16 +0200

    Cleaning

--- tools/list_audio.c
+++ tools/list_audio.c
@@ -19,11 +19,12 @@
 
 fa_audio_session_t print_audio_devices(fa_ptr_t _, audio_session_t session)
 {
+    fa_print("Listing audio devices:\n", 0);
+    fa_print_ln(string(""));
     fa_for_each(x, fa_audio_all(session)) {
-        fa_print("Name:           %s\n", fa_string_to_string(fa_audio_name(x)));
-        fa_print("Host:           %s\n", fa_string_to_string(fa_audio_host_name(x)));
-        fa_print("Input:          %s\n", i16(fa_type_channels(fa_audio_input_type(x))));
-        fa_print("Output:         %s\n", i16(fa_type_channels(fa_audio_output_type(x))));
+        fa_print("  Name: %s\n", fa_string_to_string(fa_audio_name(x)));
+        fa_print("  In:   %s\n", i16(fa_type_channels(fa_audio_input_type(x))));
+        fa_print("  Out:  %s\n", i16(fa_type_channels(fa_audio_output_type(x))));
         fa_print_ln(string(""));
     }
     return session;
