6d99312f1f1409fba86359e3ae589c4551f07e8e
Hans Höglund <hans@hanshoglund.se>
2013-10-02 02:01:46 +0200

    Lisp tests

--- bindings/lisp/test/mixer.lisp
+++ bindings/lisp/test/mixer.lisp
@@ -7,9 +7,14 @@
 (setf s (audio-open-stream* i o (lambda (_) (cl:list
                                              
   (+
-    (* (input 32) (* 0.1 (sin (line 440))))
-    (* (input 33) (* 0.1 (sin (line 550))))
-    )
+    (+
+      (* (smooth (input 32)) (* 0.1 (sin (line 440))))
+      (* (smooth (input 33)) (* 0.1 (sin (line 550))))
+      )
+    (+
+      (* (smooth (input 34)) (* 0.1 (sin (line 660))))
+      (* (smooth (input 35)) (* 0.1 (sin (line 770))))
+      ))
 
                                              ))))
 (audio-send (seconds 20) (action-set 32 0.5D0) s)
@@ -19,7 +24,29 @@
 (destroy se)
 
 
-(defvar *foo*)
+(defun smooth (x) 
+  (signal-loop* 
+   (lambda (rec)
+     (+ (* 0.999 rec)
+        (* 0.001 x)))))
+
+
+(smooth (stime))
+
+
+
+
+(defcfun (signal-loop "fa_signal_loop") signal (a unary) (b ptr))
+(defcallback signalcall1# ptr ((f ptr) (x ptr))
+  (funcall (int-to-func# f) (from-pointer 'signal x)))
+(defun signal-loop* (f)
+  (signal-loop (callback signalcall1#) (func-to-int# f)))
+
+;(from-pointer
+
+;(signal-loop* (lambda (x) (+ 1 x)))
+
+
 
 
 (progn
@@ -46,6 +73,26 @@
                   (audio-send 
                    (seconds 0) 
                    (action-set 33 x) s)))
+     (slider3 capi:slider      
+              :start 0
+              :end 100
+              :tick-frequency 0
+              :callback (lambda (interf value type) 
+                  (setf x (cl:* (coerce value 'double-float) 0.01D0))
+                  (cl:print x)
+                  (audio-send 
+                   (seconds 0) 
+                   (action-set 34 x) s)))
+     (slider4 capi:slider      
+              :start 0
+              :end 100
+              :tick-frequency 0
+              :callback (lambda (interf value type) 
+                  (setf x (cl:* (coerce value 'double-float) 0.01D0))
+                  (cl:print x)
+                  (audio-send 
+                   (seconds 0) 
+                   (action-set 35 x) s)))
      )
     (:layouts
      ; (main-layout capi:column-layout 
