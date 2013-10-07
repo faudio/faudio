d74c4a56c051b3e5063dc2f3e30ae670775710d3
Hans Höglund <hans@hanshoglund.se>
2013-10-07 11:13:48 +0200

    Adds release script

---
+++ release.sh
@@ -0,0 +1,27 @@
+VERSION=2.x.x
+
+if [[ $* != *--clean* ]]; then
+
+	rm -rf distribute
+	mkdir -p distribute
+	mkdir -p distribute/bin
+	mkdir -p distribute/include
+	# mkdir -p distribute/lib
+	mkdir -p distribute/Frameworks
+
+	cp -R include distribute
+	rm distribute/include/config.h.in
+
+	cp -R build/Frameworks/Faudio.framework distribute/Frameworks
+
+	cp -R build/bin distribute
+	rm distribute/bin/sbcl
+
+	mv distribute faudio
+	tar -pvczf faudio-$VERSION.tar.gz faudio
+	rm -rf faudio
+
+else
+	echo "Cleaning"
+	rm -rf faudio-*.tar.gz
+fi
