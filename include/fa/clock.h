dc04f493146b19c6a15d56c2e7af1cc222ed4aa3
Hans Höglund <hans@hanshoglund.se>
2013-10-10 20:12:08 +0200

    Docs

--- include/fa/clock.h
+++ include/fa/clock.h
@@ -9,7 +9,9 @@
 
     Provides various clocks.
         
-    A clock is (not surprisingly) a provider of time.
+    As one might expect, a clock is a provider of time. A clock in *faudio*,
+    however does not usually indicate real-world time, but the current time
+    of an audio computation.
 
     @par Implements
     - fa_string_show_t (all clock types)
@@ -32,10 +34,20 @@ typedef struct {
 
 typedef struct _fa_clock_t * fa_clock_t;
 
-
+/** Returns the current time according to the given clock.
+    @param clock
+        Clock to use.
+    @return
+        The current time.
+*/
 fa_time_t fa_clock_time(fa_clock_t);
 
-
+/** Returns the current time in milliseconds according to the given clock.
+    @param clock
+        Clock to use.
+    @return
+        The current time in milliseconds.
+*/
 fa_time_milliseconds_t fa_clock_milliseconds(fa_clock_t);
 
 /** @}
