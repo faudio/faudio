64719b87edda64bf3cce151b89cd277cdc9f431f
Hans Höglund <hans@hanshoglund.se>
2013-10-15 15:14:21 +0200

    OS X MIDI device detection tests
        See #40

--- tools/test_core_midi.c
+++ tools/test_core_midi.c
@@ -7,7 +7,42 @@
 /*
     This program does ...
 
- */
+ */                    
+ 
+char * MYCFStringCopyUTF8String(CFStringRef aString);
+
+void print_devices_with_status()
+{       
+
+    OSStatus status;
+    int numDevs     = MIDIGetNumberOfDevices();
+    for (int i = 0; i < numDevs; ++i) {
+        MIDIDeviceRef device = MIDIGetDevice(i);
+
+        CFStringRef name;
+        status = noErr;
+        status = MIDIObjectGetStringProperty(device, kMIDIPropertyName, &name);
+        if (/*status != noErr*/1) {                   
+        
+            char* str = MYCFStringCopyUTF8String(name);
+            printf("  Name: %s \n", str);
+            
+            // FIXME
+            // printf("  Name: %s\n", unstring(fa_string_from_native((void*) name)));
+        }
+        
+        long isOffline;
+        status = noErr;
+        status = MIDIObjectGetIntegerProperty(device, kMIDIPropertyOffline, &isOffline);
+        if (/*status != noErr*/1) {
+            printf("  Offline: %ld\n", isOffline);
+        }
+        
+        mark_used(device);
+        mark_used(status);
+    }
+}
+ 
 void print_num_devices()
 {
     int numDevs     = MIDIGetNumberOfDevices();
@@ -32,7 +67,15 @@ void midi_listener(const MIDINotification *message, void *data)
 }
 
 
-void run_external_loop()
+void threaded_assert(int status, char* msg)
+{
+    if (!status) {
+        fa_fa_log_error(string(msg));
+    }
+}
+
+
+ptr_t run_external_loop(ptr_t _)
 {
     fa_print_ln(string("Running the external loop"));
 
@@ -41,14 +84,15 @@ void run_external_loop()
 
     MIDIClientRef client;
     status = MIDIClientCreate(name, midi_listener, NULL, &client);
-    assert ((status == noErr) && "MIDIClientCreate failed");
+    threaded_assert ((status == noErr), "MIDIClientCreate failed");
 
     CFRunLoopRun();
     mark_used(client);
+    assert(false && "Unreachable");
 }
 
 
-void run_internal_loop()
+ptr_t run_internal_loop(ptr_t _)
 {
     fa_print_ln(string("Running the internal loop"));
 
@@ -65,6 +109,7 @@ void run_internal_loop()
         assert ((status == noErr) && "MIDIClientCreate failed");
 
         print_num_devices();
+        print_devices_with_status();
 
         status = MIDIClientDispose(client);
         assert ((status == noErr) && "MIDIClientDispose failed");
@@ -72,14 +117,44 @@ void run_internal_loop()
         fa_thread_sleep(500);
         mark_used(status);
     }
+    assert(false && "Unreachable");
 }
 
 int main(int argc, char const *argv[])
-{
+{            
+    fa_fa_set_log_std();
     fa_fa_initialize();
 
-    // run_internal_loop();
-    run_external_loop();
+    run_internal_loop(NULL);
+    // run_external_loop(NULL);
+    // fa_thread_create(run_external_loop, NULL);
 
     fa_fa_terminate();
+}
+
+
+
+
+
+
+
+
+// TODO
+
+
+char * MYCFStringCopyUTF8String(CFStringRef aString) {
+  if (aString == NULL) {
+    return NULL;
+  }
+
+  CFIndex length = CFStringGetLength(aString);
+  CFIndex maxSize =
+  CFStringGetMaximumSizeForEncoding(length,
+                                    kCFStringEncodingUTF8);
+  char *buffer = (char *)malloc(maxSize);
+  if (CFStringGetCString(aString, buffer, maxSize,
+                         kCFStringEncodingUTF8)) {
+    return buffer;
+  }
+  return NULL;
 }
