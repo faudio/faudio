686a09e69cc69e7502226242af4b30b6690d4490
Patrik <soundbuilderandroid@gmail.com>
2013-10-18 12:19:50 +0200

    Implemented 32-bit atomic functions for Windows
    Some testing is needed

--- platform/windows/atomic.c
+++ platform/windows/atomic.c
@@ -6,7 +6,9 @@
  */
 
 #include <fa/atomic.h>
-#include <fa/util.h>    // for impl_t
+#include <fa/string.h>
+#include <fa/util.h>
+
 #include <Windows.h>
 
 /*
@@ -17,47 +19,133 @@ struct _fa_atomic_t {
     intptr_t    value;
 };
 
+fa_ptr_t atomic_impl(fa_id_t);
+
 fa_atomic_t fa_atomic_create()
 {
-    fa_atomic_t a = malloc(sizeof(struct _fa_atomic_t));
-    return a;
+	atomic_t a = fa_new(atomic);
+	a->impl 	= &atomic_impl;
+	a->value 	= NULL;
+
+	return a;
 }
 
 fa_atomic_t fa_atomic_copy(fa_atomic_t a)
 {
-    free(a);
+    atomic_t b 	= fa_atomic_create();
+	b->value	= a->value;
+	return b;
 }
 
-void fa_atomic_swap(fa_atomic_t a, fa_atomic_t b)
-{
-}
+/*
+void fa_atomic_swap(fa_atomic_t a, fa_atomic_t b){}
+*/
 
 void fa_atomic_destroy(fa_atomic_t a)
 {
+	fa_delete(a);
 }
 
 bool fa_atomic_exchange(fa_atomic_t a, fa_ptr_t pold, fa_ptr_t pnew)
 {
+	return (pold == InterlockedCompareExchangePointer((LONG*)&a->value, (LONG) pnew, (LONG) pold));
 }
 
 void fa_atomic_add(fa_atomic_t a, intptr_t v)
 {
+	InterlockedExchangeAdd((LONG*)&a->value, (LONG)v);
 }
 
 void* fa_atomic_get(fa_atomic_t a)
 {
+	InterlockedCompareExchange((LONG*)&a->value, 0L, 0L);
 }
 
 void fa_atomic_modify(fa_atomic_t atomic, fa_unary_t func, fa_ptr_t data)
 {
+	bool result = FALSE;
+
+	while(!result) {
+		ptr_t state = fa_atomic_get(atomic);
+		ptr_t value = func(state, data);
+		result = fa_atomic_exchange(atomic, state, value);
+	}
 }
 
-void fa_atomic_set(fa_atomic_t a, fa_ptr_t v)
+void fa_atomic_set(fa_atomic_t atomic, fa_ptr_t value)
 {
+	bool result = FALSE;
+
+	while(!result) {
+		ptr_t state = fa_atomic_get(atomic);
+		result = fa_atomic_exchange(atomic, state, value);
+	}
 }
 
+// --------------------------------------------------------------------------------
 
+bool atomic_equal(fa_ptr_t a, fa_ptr_t b)
+{
+    return fa_equal(fa_atomic_get(a), fa_atomic_get(b));
+}
 
+bool atomic_less_than(fa_ptr_t a, fa_ptr_t b)
+{
+    return fa_less_than(fa_atomic_get(a), fa_atomic_get(b));
+}
+
+bool atomic_greater_than(fa_ptr_t a, fa_ptr_t b)
+{
+    return fa_greater_than(fa_atomic_get(a), fa_atomic_get(b));
+}
+
+fa_string_t atomic_show(fa_ptr_t v)
+{
+    fa_atomic_t a = (fa_atomic_t) v;
+    string_t s = string("<Atomic");
+    s = string_dappend(s, fa_string_format_integral(" %02x", (LONG) a->value));
+    s = string_dappend(s, string(">"));
+    return s;
+}
+
+fa_ptr_t atomic_copy(fa_ptr_t a)
+{
+    return fa_atomic_copy(a);
+}
+
+void atomic_destroy(fa_ptr_t a)
+{
+    fa_atomic_destroy(a);
+}
+
+fa_ptr_t atomic_impl(fa_id_t iface)
+{
+    static fa_equal_t atomic_equal_impl = { atomic_equal };
+    static fa_order_t atomic_order_impl = { atomic_less_than, atomic_greater_than };
+    static fa_string_show_t atomic_show_impl = { atomic_show };
+    static fa_copy_t atomic_copy_impl = { atomic_copy };
+    static fa_destroy_t atomic_destroy_impl = { atomic_destroy };
+
+    switch (iface) {
+    case fa_equal_i:
+        return &atomic_equal_impl;
+
+    case fa_order_i:
+        return &atomic_order_impl;
+
+    case fa_string_show_i:
+        return &atomic_show_impl;
+
+    case fa_copy_i:
+        return &atomic_copy_impl;
+
+    case fa_destroy_i:
+        return &atomic_destroy_impl;
+
+    default:
+        return NULL;
+    }
+}
 
 /*
   #ifdef SCL_ATOMIC_OSX
