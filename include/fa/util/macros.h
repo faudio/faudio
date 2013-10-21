baf34b0356b19c6967c77809845e12adb611824b
Hans Höglund <hans@hanshoglund.se>
2013-10-21 15:36:22 +0200

    Cleans util/macro

--- include/fa/util/macros.h
+++ include/fa/util/macros.h
@@ -2,19 +2,30 @@
 #ifndef _FA_UTIL_MACROS
 #define _FA_UTIL_MACROS
 
+/** @defgroup Fa Fa
+    @{
+    @defgroup FaUtility Utility
+    @{
+
+ */
+
 /** Execute following statement with a binding in scope.
 
-    Syntax:
+    ### Syntax
 
-        fa_let(var, expr)
-            statement;
+    ~~~
+    fa_let(var, expr)
+        statement;
+    ~~~
 
-    Example:
+    ### Example
 
-        fa_let(x, 23)
-        {
-            printf("%d\n", 23)
-        }
+    ~~~
+    fa_let(x, 23)
+    {
+        printf("%d\n", 23)
+    }
+    ~~~
  */
 #define fa_let(var, expr) \
     for (__typeof__(expr) var = expr, *__c=((__typeof__(expr)*) 1); \
@@ -23,23 +34,26 @@
          )
 
 /** Execute the following statement with a binding in scope, then
-    evaluate the given cleanup finalizer expression.
+    evaluate the given finalizer expression.
 
     You can escape with-context with either continue or break: note that
     continue *will* execute the finalizer expression, break will *not*.
 
-    Syntax:
+    ### Syntax
 
-        fa_with(var, expr, final)
-            statement;
+    ~~~
+    fa_with(var, expr, final_expr)
+        statement;
+    ~~~
 
-    Example:
-
-        fa_with(x, get_resource(), release_resource(x))
-        {
-            use_resource(x);
-        }
-
+    ### Example
+    
+    ~~~
+    fa_with(x, get_resource(), release_resource(x))
+    {
+        use_resource(x);
+    }
+    ~~~
  */
 #define fa_with(var, expr, destr) \
     for (__typeof__(expr) var = expr, *__c=((__typeof__(expr)*) 1); \
@@ -52,18 +66,21 @@
     You can escape with-context with either continue or break: note that
     continue *will* execute the finalizer expression, break will *not*.
 
-    Syntax:
+    ### Syntax
 
-        fa_with(var, expr)
-            statement;
+    ~~~
+    fa_with(var, expr)
+        statement;
+    ~~~
 
-    Example:
+    ### Example
 
-        fa_with(x, get_resource())
-        {
-            use_resource(x);
-        }
-
+    ~~~
+    fa_with(x, get_resource())
+    {
+        use_resource(x);
+    }
+    ~~~
  */
 #define fa_with_temp(var, expr) \
     fa_with(var, expr, fa_destroy(var))
@@ -71,20 +88,21 @@
 /** Execute the following statement once for each item in the
     given list.
 
-    Syntax:
+    ### Syntax
 
-        fa_for_each(var, list)
-            statement;
+    ~~~
+    fa_for_each(var, list)
+        statement;
+    ~~~
 
-    Example:
+    ### Example
 
-        fa_dloop(
-            ptr_t, x, list(1,2,3)
-        )
-        {
-            use_resource(x);
-        }
-
+    ~~~
+    fa_for_each(x, list(1,2,3))
+    {
+        use_resource(x);
+    }
+    ~~~
  */
 #define fa_for_each(var, list) \
     fa_dfor_each(var, fa_list_copy(list))
@@ -92,20 +110,21 @@
 /** Execute the following statement once for each item in the
     given list (destructive).
 
-    Syntax:
+    ### Syntax
 
-        fa_for_each(var, list)
-            statement;
+    ~~~
+    fa_dfor_each(var, list)
+        statement;
+    ~~~
 
-    Example:
+    ### Example
 
-        fa_dloop(
-            ptr_t, x, list(1,2,3)
-        )
-        {
-            use_resource(x);
-        }
-
+    ~~~
+    fa_dfor_each(x, list(1,2,3))
+    {
+        use_resource(x);
+    }
+    ~~~
  */
 #define fa_dfor_each(var, list) \
     fa_with (__j, list, fa_list_destroy(__j)) \
@@ -141,23 +160,33 @@
     You can escape locking context with either continue or break: note that
     continue *will* release the lock, break will *not*.
 
-    Syntax:
+    ### Syntax
 
-        fa_with_mutex(mutex)
-            statement;
+    ~~~
+    fa_with_lock(mutex)
+        statement;
+    ~~~
+    
+    ### Example
 
-    Example:
-
-        fa_let(x, 23)
-        {
-            printf("%d\n", 23)
+    ~~~
+    fa_with_lock(myMutex)
+    {
+        if (error_condition()) {
+            continue;
         }
+        do_syncronized_stuff();
+    }
+    ~~~
  */
 #define fa_with_lock(mutex) \
     fa_with(__mutex, \
         fa_thread_lock(mutex), \
         fa_thread_unlock((void*) mutex + 0*((intptr_t)__mutex)))
     
+/** @}
+    @}
+    */
 
 #endif // _FA_UTIL_MACROS
 
