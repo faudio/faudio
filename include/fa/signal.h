90fb048b53e165fe36b1fd66d76f50b4b365909d
Hans Höglund <hans@hanshoglund.se>
2013-10-23 17:38:29 +0200

    Adds stream-based I/O API (TODO implement)
    See #54

--- include/fa/signal.h
+++ include/fa/signal.h
@@ -180,6 +180,23 @@ fa_signal_t fa_signal_latter(fa_signal_t, fa_signal_t);
 */
 fa_signal_t fa_signal_former(fa_signal_t, fa_signal_t);
 
+
+typedef void (* fa_signal_stream_input_callback_t)(size_t,
+                                                   fa_ptr_t);
+
+
+typedef void (* fa_signal_stream_output_callback_t)(size_t,
+                                                    fa_ptr_t);
+
+
+fa_signal_t fa_signal_input_stream(int,
+                                   fa_signal_stream_input_callback_t);
+
+
+fa_signal_t fa_signal_output_stream(int,
+                                    fa_signal_stream_output_callback_t,
+                                    fa_signal_t);
+
 /** Run the given signal for *n* samples, printing the values to `stdout`.
 */
 void fa_signal_print(int, fa_list_t, fa_signal_t);
