8b527e6e1b47964b7cc2fe7b7d61c21b6dcb9a3e
Hans Höglund <hans@hanshoglund.se>
2013-11-11 17:57:37 +0100

    Cleaning

--- platform/macosx/signal.c
+++ platform/macosx/signal.c
@@ -42,7 +42,6 @@ ptr_t render_(ptr_t x, fa_signal_state_t *state)
 {
     au_context_t context = x;
 
-    // context->Time = state->count;
     if (state->count % kAUVectorSize == 0) {
         au_render(context, state->count, NULL);
     }
@@ -58,9 +57,8 @@ ptr_t receive_(ptr_t x, fa_signal_name_t n, fa_signal_message_t msg)
 {
     au_context_t context = x;
     assert(fa_midi_message_is_simple(msg));
-    // printf("Message: %s\n", unstring(fa_string_show(msg)));
 
-    // TODO pre-allocate
+    // TODO
     if (true /*fa_equal(n, string("DLS"))*/) {
         int status, data1, data2;
         fa_midi_message_decons(msg, &status, &data1, &data2);
@@ -83,8 +81,6 @@ pair_t fa_signal_dls()
     proc->render  = render_;
     proc->receive = receive_;
     proc->data    = context;
-
-    // printf("Sending custom proc %p!\n", proc);
 
     signal_t left  = fa_signal_input(kAUOffset + 0);
     signal_t right = fa_signal_input(kAUOffset + 1);
