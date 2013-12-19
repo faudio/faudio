b4af4cfbba887a6a3a856952fcf948b91df57d79
Hans Höglund <hans@hanshoglund.se>
2013-12-19 16:51:31 +0100

    Removing auto-generated language bindings from repo

--- bindings/lisp/fa/dynamic.lisp
+++
@@ -1,6 +0,0 @@
-(in-package :faudio)
-(defctype dynamic-type-repr :int)
-(defctype dynamic (:pointer :void))
-(defcfun (dynamic-check "fa_dynamic_check") :boolean (a ptr))
-(defcfun (dynamic-get-type "fa_dynamic_get_type") dynamic-type-repr (a ptr))
-
--- bindings/lisp/fa/system/directory.lisp
+++
@@ -1,9 +0,0 @@
-(in-package :faudio)
-(defcfun (system-directory-home "fa_system_directory_home") string)
-(defcfun (system-directory-current "fa_system_directory_current") string)
-(defcfun (system-directory-create "fa_system_directory_create") :void (a string))
-(defcfun (system-directory-remove "fa_system_directory_remove") :void (a string))
-(defcfun (system-directory-read-file "fa_system_directory_read_file") string (a string))
-(defcfun (system-directory-write-file "fa_system_directory_write_file") :void (a string) (b string))
-(defcfun (system-directory-append-file "fa_system_directory_append_file") :void (a string) (b string))
-
