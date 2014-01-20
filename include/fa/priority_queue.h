b9ec12225f1243394cf1399c8580f33b948fa957
Hans Höglund <hans@hanshoglund.se>
2014-01-20 20:42:04 +0100

    Docs

--- include/fa/priority_queue.h
+++ include/fa/priority_queue.h
@@ -28,26 +28,37 @@
 
 typedef struct _fa_priority_queue_t * fa_priority_queue_t;
 
-
+/**  Create an empty queue. 
+*/
 fa_priority_queue_t fa_priority_queue_empty();
 
-
+/**  Create an queue containing a single element. 
+*/
 fa_priority_queue_t fa_priority_queue_single(fa_ptr_t);
 
-
+/**  Destroy the given queue. 
+*/
 void fa_priority_queue_destroy(fa_priority_queue_t);
 
-
+/**  Merge the two given queues into the first. 
+*/
 void fa_priority_queue_merge(fa_priority_queue_t,
                              fa_priority_queue_t);
 
-
+/**  Insert the given element into the given queue. 
+*/
 void fa_priority_queue_insert(fa_ptr_t, fa_priority_queue_t);
 
-
+/**  Returns the top-most element in the given queue, if any.
+     @errors
+        Returns `NULL` if the queue is empty.
+*/
 fa_ptr_t fa_priority_queue_peek(fa_priority_queue_t);
 
-
+/**  Returns the top-most element and remove it from the queue.
+     @errors
+        Returns `NULL` if the queue is empty.
+*/
 fa_ptr_t fa_priority_queue_pop(fa_priority_queue_t);
 
 /** @}
