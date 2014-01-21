7d7f9c01c756d4978bd8521fbe2f9d97f1304d8c
Hans Höglund <hans@hanshoglund.se>
2014-01-21 15:23:10 +0100

    Adds options to fa_sine (experimental)

--- tools/sine.c
+++ tools/sine.c
@@ -1,6 +1,7 @@
 
 #include <fa/fa.h>
 #include <fa/util.h>
+#include <fa/option.h>
 
 /*
     This program plays one of more sine waves on the standard audio output device.
@@ -15,10 +16,23 @@ list_t just(ptr_t x, list_t xs)
     return x;
 }
 
-void helper_function()
+#define fa_unpair(P,A,B) \
+    fa_let(__p, P) \
+    fa_let(A,fa_pair_first(__p)) \
+    fa_let(B,fa_pair_second(__p))
+// TODO move
+#define fa_sizeof_array(A) sizeof(A) / sizeof(A[0])
+#define fa_option_show_all(A,S) fa_option_show(fa_sizeof_array(A),A,S)
+#define fa_option_parse_all(A,AC,AV) fa_option_parse(fa_sizeof_array(A), A, AC, AV)
+
+fa_option_t options[] = {
+    { "f", "frequency", "Frequency (default 440)",  fa_option_parse_int },
+};
+
+void helper_function(int freq1, int freq2)
 {
     signal_t b = fa_multiply(fa_signal_sin(fa_signal_line(220)), constant(0.1));
-    signal_t c = fa_multiply(fa_signal_sin(fa_signal_line(330)), constant(0.1));
+    signal_t c = fa_multiply(fa_signal_sin(fa_signal_line(freq2)), constant(0.1));
     signal_t a = fa_add(b, c);
     // signal_t a = fa_multiply(fa_signal_random(), constant(0.1));
 
@@ -48,6 +62,13 @@ int main(int argc, char const *argv[])
 {
     fa_fa_set_log_std();
     fa_fa_initialize();
-    helper_function();
+
+    fa_unpair(fa_option_parse_all(options, argc, (char**) argv), opts, _)
+    {
+        mark_used(_);
+        int freq = fa_map_get(string("frequency"), opts) ? fa_peek_int32(fa_map_get(string("frequency"), opts)) : 220;
+        helper_function(freq,(freq*3/2));
+    }
+
     fa_fa_terminate();
 }
