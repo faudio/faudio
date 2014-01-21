a997f8c64301a1c731c5b9680951472a0a8ff7c7
Hans Höglund <hans@hanshoglund.se>
2014-01-21 13:19:00 +0100

    API

--- include/fa/option.h
+++ include/fa/option.h
@@ -8,8 +8,41 @@
 #include <fa/pair.h>
 
 /** @addtogroup FaOption
+ 
 
-    Basic options parser.
+Basic options parser.
+    
+Example:
+
+~~~
+fa_option_t options[] = {
+    { "h", "help",            "Show help info",             fa_option_parse_string },
+    { "x", "value-with-def",  "Show help info",             fa_option_parse_int    },
+    { "n", "number-of-cores", "Number of cores\n                                   "
+                              "Very interesting parameter", fa_option_parse_int    }
+};
+
+int main(int argc, char const *argv[])
+{
+    fa_option_show(
+        sizeof(options) / sizeof(fa_option_t), options,
+        "Usage: fa_options\n"
+        "       fa_options [FILES]\n"
+    );
+                
+    
+    fa_unpair(
+        fa_option_parse(sizeof(options) / sizeof(fa_option_t), options, argc, (char**) argv), 
+        options,
+        arguments
+        ) 
+    {
+        fa_print_ln(options);   // {"foo": 1, "bar": 2}
+        fa_print_ln(arguments); // [1,2,3]
+    }
+}
+~~~    
+    
  
     @defgroup Fa Fa
     @{
@@ -26,7 +59,7 @@ typedef struct {
         } fa_option_t;
 
 
-fa_ptr_t fa_option_parse(int, fa_option_t [1], int, char * *);
+fa_pair_t fa_option_parse(int, fa_option_t [1], int, char * *);
 
 
 void fa_option_show(int, fa_option_t [1], char *);
