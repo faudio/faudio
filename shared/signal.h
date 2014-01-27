ab34cff212423bef16905c278a210e24fd63fcd4
Hans Höglund <hans@hanshoglund.se>
2014-01-27 16:25:35 +0100

    Documents internal API
    See #167

--- shared/signal.h
+++ shared/signal.h
@@ -33,11 +33,22 @@ struct _state_base_t {
 typedef struct _state_t *state_t;
 typedef struct _state_base_t *state_base_t;
 
-// ?
-list_t fa_signal_get_procs(fa_signal_t signal2);
+
+
+
+
+
+
+// TODO hide all of these by rewriting signal/audio to use callbacks
+
+/** Create a new DSP state object,
+ */
 state_t new_state(int sample_rate);
-void add_custom_proc(fa_signal_custom_processor_t* proc, state_t state);
+
+/** Destroy a DSP state object,
+ */
 void delete_state(state_t state);
+
 void inc_state1(state_t state);           
 void inc_state(int n, state_t state);           
 
@@ -45,9 +56,16 @@ typedef enum { custom_proc_before,
        custom_proc_render,
        custom_proc_after } custom_proc_when_t;
        
+/** Extract a list of fa_signal_custom_processor_t
+    To be added with add_custom_proc
+ */
+list_t fa_signal_get_procs(fa_signal_t signal2);
+void add_custom_proc(fa_signal_custom_processor_t* proc, state_t state);
 void run_custom_procs(custom_proc_when_t when, state_t state);
 
+/** Run a simpel action (send, set or accum) on the processing state. */
 ptr_t run_simple_action(state_t state, fa_action_t action);
+
 double step(signal_t signal, state_t state);
 void step_vector(signal_t signal, state_t state, int count, double* out);
-fa_signal_t fa_signal_simplify(fa_signal_t signal2);
+
