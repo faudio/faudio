48ae0a2e2cd04c99dd6f55157bbe599ae177d386
Hans Höglund <hans@hanshoglund.se>
2014-01-29 20:28:11 +0100

    Updated docs

---
+++ docs/group___fa_ratio.js
@@ -0,0 +1,23 @@
+var group___fa_ratio =
+[
+    [ "fa_ratio_num_t", "group___fa_ratio.html#ga6d6962b946d96535558e341030f16e07", null ],
+    [ "fa_ratio_denom_t", "group___fa_ratio.html#ga18626f0c26ea1666ce0725605044cfa1", null ],
+    [ "fa_ratio_t", "group___fa_ratio.html#gaf3b37b5fdfcccb6283b7ac806c72b273", null ],
+    [ "fa_ratio_create", "group___fa_ratio.html#ga3ab0d3fea73d05b4173e066f954389a5", null ],
+    [ "fa_ratio_num", "group___fa_ratio.html#gaa94661f5ff09ed1a547f870f6e1c5a9b", null ],
+    [ "fa_ratio_denom", "group___fa_ratio.html#ga71fb6b16111090ebef75fce0a62eb234", null ],
+    [ "fa_ratio_match", "group___fa_ratio.html#gacb1ec6f18e009d96c398f83e7a507270", null ],
+    [ "fa_ratio_copy", "group___fa_ratio.html#ga240d745928d4f3ea0f03930617580f62", null ],
+    [ "fa_ratio_destroy", "group___fa_ratio.html#gaa65ebd7b8a36f340bf5d54ff8d14738e", null ],
+    [ "fa_ratio_add", "group___fa_ratio.html#ga6f21aa6cbc2216a1277f2158968ce52b", null ],
+    [ "fa_ratio_subtract", "group___fa_ratio.html#ga4532996b003dc8a628053dd8d159cf82", null ],
+    [ "fa_ratio_multiply", "group___fa_ratio.html#gaeeef9bb9a4a888e53e7fbe09ff5c5dd0", null ],
+    [ "fa_ratio_divide", "group___fa_ratio.html#ga5995de6035f8f7f95c645dfd6d1b3297", null ],
+    [ "fa_ratio_succ", "group___fa_ratio.html#ga7c72bad7eef5ca15eb888b08ed744ed0", null ],
+    [ "fa_ratio_pred", "group___fa_ratio.html#ga78052a323d41d5d76a526e1e19ba0eb2", null ],
+    [ "fa_ratio_negate", "group___fa_ratio.html#ga39bc43bc50d936309bf763b6903249bd", null ],
+    [ "fa_ratio_recip", "group___fa_ratio.html#gae2a0b5d0ca551b62a4a8edc6b399b742", null ],
+    [ "fa_ratio_absolute", "group___fa_ratio.html#gae3545b10373eeba8414a490e5d9d0ec8", null ],
+    [ "fa_ratio_normalize", "group___fa_ratio.html#gad477d6485fd4e6ff3bca8bda977447ff", null ],
+    [ "fa_ratio_to_mixed", "group___fa_ratio.html#ga13dc172c048dcd34c04aedd20f0fe1d6", null ]
+];
---
+++ docs/group___fa_system.js
@@ -0,0 +1,4 @@
+var group___fa_system =
+[
+    [ "Directory", "group___fa_system_directory.html", "group___fa_system_directory" ]
+];
