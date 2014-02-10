eb21ceabe02dd481199e08ee345e6d14bd473c70
Hans Höglund <hans@hanshoglund.se>
2014-02-10 15:54:38 +0100

    Updated docs

---
+++ versions/2.9.4/docs/group___fa_option.js
@@ -0,0 +1,13 @@
+var group___fa_option =
+[
+    [ "fa_option_t", "structfa__option__t.html", null ],
+    [ "fa_option_parser_t", "group___fa_option.html#ga86aa8e13dfaaa7e6870855b204cfe3a0", null ],
+    [ "fa_option_integral", "group___fa_option.html#ga9ba72e31cecff0d13f4ad97a76f84c84", null ],
+    [ "fa_option_floating", "group___fa_option.html#gaf40e446b8873176ee62f74be2822e1e7", null ],
+    [ "fa_option_string", "group___fa_option.html#ga8bb94c18d6373f7245188120dcb70562", null ],
+    [ "fa_option_failure", "group___fa_option.html#gaf46a405a8c9ed29f3f8177e189262425", null ],
+    [ "fa_option_parse_all", "group___fa_option.html#gaf74fe1a41763c353bf8fa0ed7f4259c6", null ],
+    [ "fa_option_show_all", "group___fa_option.html#ga35db7ce1235a1061817436bedb1b39d6", null ],
+    [ "fa_option_parse", "group___fa_option.html#ga8a6e4ac1997796f4fab0d787a346fbe8", null ],
+    [ "fa_option_show", "group___fa_option.html#gaf0e5db771026f5b3e2e662927e3ed62b", null ]
+];
