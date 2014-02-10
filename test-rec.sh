f20c39b17da4665eeb0bb901a61330e13ba558bb
Hans Höglund <hans@hanshoglund.se>
2014-02-10 15:35:07 +0100

    Adds RAW streaming tests

--- test-rec.sh
+++ test-rec.sh
@@ -5,24 +5,24 @@ EASY="sleep 2"
 rm -rf rectest
 mkdir rectest
 
-# build/bin/fa_record_streaming -o rectest/app_id1.raw
-# $EASY
-# build/bin/fa_record_streaming -o rectest/app_id2.raw
-# $EASY
-# build/bin/fa_record_streaming -o rectest/app_id3.raw
-# $EASY
-# build/bin/fa_record_streaming -o rectest/app_id4.raw
-# $EASY
-# build/bin/fa_record_streaming -o rectest/app_id5.raw
-# $EASY
-# build/bin/fa_record_streaming -o rectest/app_id6.raw
-# $EASY
-# build/bin/fa_record_streaming -o rectest/app_id7.raw
-# $EASY
-# build/bin/fa_record_streaming -o rectest/app_id8.raw
-# $EASY
-# build/bin/fa_record_streaming -o rectest/app_id9.raw
-# $EASY
+build/bin/fa_record_streaming -o rectest/app_id1.raw
+$EASY
+build/bin/fa_record_streaming -o rectest/app_id2.raw
+$EASY
+build/bin/fa_record_streaming -o rectest/app_id3.raw
+$EASY
+build/bin/fa_record_streaming -o rectest/app_id4.raw
+$EASY
+build/bin/fa_record_streaming -o rectest/app_id5.raw
+$EASY
+build/bin/fa_record_streaming -o rectest/app_id6.raw
+$EASY
+build/bin/fa_record_streaming -o rectest/app_id7.raw
+$EASY
+build/bin/fa_record_streaming -o rectest/app_id8.raw
+$EASY
+build/bin/fa_record_streaming -o rectest/app_id9.raw
+$EASY
 
 build/bin/fa_record_streaming --ogg-vorbis -o rectest/app_ogg1.ogg
 $EASY
