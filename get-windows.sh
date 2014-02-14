7e6667d0e48e1287aae3849ca474c1a2a36340fb
Hans Höglund <hans@hanshoglund.se>
2014-02-14 11:59:48 +0100

    RELEASE

--- get-windows.sh
+++ get-windows.sh
@@ -1,3 +1,11 @@
-
 #!/bin/bash
-scp hans@192.168.0.69:/c/Users/Doremir/Documents/bygge/faudio/faudio-2.9.3.zip .
+
+if [ $# -lt 1 ]
+then
+  echo "Usage: `basename $0` [version]"
+  exit $E_BADARGS
+else
+    VERSION=$1
+fi
+
+scp hans@192.168.0.69:/c/Users/Doremir/Documents/bygge/faudio/faudio-$VERSION.zip .
