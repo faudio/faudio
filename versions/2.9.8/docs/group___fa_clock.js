7bdd6592d0afa34d8a2541a8b2508d26ba4e7294
Hans Höglund <hans@hanshoglund.se>
2014-02-18 14:30:23 +0100

    Updated docs

---
+++ versions/2.9.8/docs/group___fa_clock.js
@@ -0,0 +1,8 @@
+var group___fa_clock =
+[
+    [ "fa_clock_interface_t", "structfa__clock__interface__t.html", null ],
+    [ "fa_clock_t", "group___fa_clock.html#ga20b3a0f49788fbedba140b1d315d2313", null ],
+    [ "fa_clock_time", "group___fa_clock.html#ga18d53d2bcd2ff1618760b9960e40d56d", null ],
+    [ "fa_clock_milliseconds", "group___fa_clock.html#gad09133a7293cd31a05d99b8915546a4a", null ],
+    [ "fa_clock_standard", "group___fa_clock.html#ga867297b309d28fdf6e47bf259eb6f914", null ]
+];
---
+++ versions/2.9.8/docs/group___fa_plot.js
@@ -0,0 +1,8 @@
+var group___fa_plot =
+[
+    [ "fa_plot_function_t", "group___fa_plot.html#ga6c91e512a0be9ba8332e0023697c8c8f", null ],
+    [ "fa_plot_use_gnu", "group___fa_plot.html#ga016706d500d136cf55d2b01002163de7", null ],
+    [ "fa_plot_continous", "group___fa_plot.html#ga285506e3a3a8cdba5ecad4f3a70a6833", null ],
+    [ "fa_plot_buffer_float", "group___fa_plot.html#ga07bfe0fbe9280762eca9d2f3204ba2ee", null ],
+    [ "fa_plot_buffer_double", "group___fa_plot.html#ga245d4644c4b5a90497ab27b46c3ceaf2", null ]
+];
