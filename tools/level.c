0d05eaf236ab265a122af1dc0c3078c709337e4e
Hans Höglund <hans@hanshoglund.se>
2014-03-06 12:51:23 +0100

    Level monitor #180
    - Basic sketch

--- tools/level.c
+++ tools/level.c
@@ -11,14 +11,38 @@
 
 #define RT 1
 #define kThisPlugOffset 32 // TODO
+#define kInputOffset 8 // FIXME
 
-static bool should_click = false;
-
+int processed = 0;
+static bool should_send = false;
+double values[2];
 
 list_t just(ptr_t x, list_t _)
 {
     return x;
 }
+
+
+/*
+    (defun smooth (x)
+      (signal-loop*
+       (lambda (rec)
+         (+ (* 0.999 rec)
+            (* 0.001 x)))))
+*/             
+signal_t smooth2_(ptr_t xx, signal_t r) {
+    signal_t x = xx;
+    return fa_signal_max(fa_signal_add(
+        fa_signal_multiply(constant(0.999), r),
+        fa_signal_multiply(constant(0.001), x)
+        ),
+        x);
+} 
+signal_t smooth2(signal_t x)
+{
+    return fa_signal_loop(smooth2_, x);
+}
+
 
 static string_t out_name;
 static ptr_t    out_value;
@@ -37,24 +61,22 @@ ptr_t after_(ptr_t x, int count, fa_signal_state_t *state)
 ptr_t render_(ptr_t x, int count, fa_signal_state_t *state)
 {
     if (!kVectorMode) {
-        state->buffer[(kThisPlugOffset + 0)*kMaxVectorSize] = should_click;
-        state->buffer[(kThisPlugOffset + 1)*kMaxVectorSize] = should_click;
-        should_click = false;
+        values[0] = state->buffer[(kThisPlugOffset + 0)*kMaxVectorSize];
+        values[1] = state->buffer[(kThisPlugOffset + 1)*kMaxVectorSize];
+        should_send = should_send || (!(state->count % (44100/100)));
     } else {
-        for (int i = 0; i < count; ++i) {
-            state->buffer[(kThisPlugOffset + 0)*kMaxVectorSize + i] = should_click;
-            state->buffer[(kThisPlugOffset + 1)*kMaxVectorSize + i] = should_click;
-            should_click = false;
-        }
+        values[0] = state->buffer[(kThisPlugOffset + 0)*kMaxVectorSize + 0];
+        values[1] = state->buffer[(kThisPlugOffset + 1)*kMaxVectorSize + 0];
+        // TODO should_send
     }
+
 
     return x;
 }
 
 ptr_t receive_(ptr_t x, fa_signal_name_t n, fa_signal_message_t msg)
 {
-    printf("Click!\n");
-    should_click = true;
+    // should_send = true;
     return x;
 }
 
@@ -63,12 +85,15 @@ ptr_t send_(ptr_t x, fa_signal_message_callback_t cb, ptr_t data)
     // TODO should not allocate here
     // The value passed is *not* destroyed, but *will* be copied
     // All create/destroy should happen in the setup phase
-    cb(data, out_name, out_value);
+    if (should_send) {
+        should_send = false;
+        cb(data, out_name, f32(values[0]));        
+    }
     return x;
 }
 
 
-pair_t fa_signal_clicks()
+pair_t fa_signal_level(signal_t a, signal_t b)
 {
     fa_signal_custom_processor_t *proc = fa_malloc(sizeof(fa_signal_custom_processor_t));
     proc->before  = before_;
@@ -78,7 +103,11 @@ pair_t fa_signal_clicks()
     proc->send    = send_;
     proc->data    = NULL;
 
-    return pair(fa_signal_custom(proc, fa_signal_input(kThisPlugOffset + 0)), fa_signal_input(kThisPlugOffset + 1));
+    return pair(fa_signal_custom(proc, 
+        fa_signal_latter(fa_signal_output(0, kThisPlugOffset + 0, smooth2(fa_signal_absolute(a))), a)
+        ), 
+        fa_signal_latter(fa_signal_output(0, kThisPlugOffset + 1, b), b)
+        );
 }
 
 ptr_t _message_out(ptr_t x, ptr_t name, ptr_t value)
@@ -86,28 +115,44 @@ ptr_t _message_out(ptr_t x, ptr_t name, ptr_t value)
     fa_print("Receieved 1: %s\n", pair(name, value));
     return x;
 }
+
 ptr_t _message_out2(ptr_t x, ptr_t name, ptr_t value)
 {
-    fa_print("Receieved 2: %s\n", pair(name, value));
+    // fa_print("Receieved 2: %s\n", pair(name, value));
+    int bars = (int) (fa_peek_double(value) * 80);
+
+    for (int i = 0; i < 80; ++i) {
+        if ( i < bars)
+            printf("#");  
+        else
+            printf (" ");
+    }
+    printf("\r");
+    fflush(stdout);
+    
     return x;
 }
-void run_clicks()
+void run_level()
 {
     if (RT) {
         fa_audio_session_t s = fa_audio_begin_session();
         fa_audio_device_t i  = fa_audio_default_input(s);
         fa_audio_device_t o  = fa_audio_default_output(s);
-        list_t out           = fa_pair_to_list(fa_signal_clicks());
+        list_t out           = fa_pair_to_list(fa_signal_level(
+            // fa_signal_sin(fa_signal_line(0.1)),
+            fa_signal_input(kInputOffset + 0), 
+            
+            fa_signal_input(kInputOffset + 1)));
 
         fa_audio_stream_t st = fa_audio_open_stream(i, o, just, out);
-        fa_audio_add_message_callback(_message_out, NULL, st);
-        // fa_audio_add_message_callback(_message_out2, NULL, st);
+        // fa_audio_add_message_callback(_message_out, NULL, st);
+        fa_audio_add_message_callback(_message_out2, NULL, st);
 
         if (fa_check(st)) {
             fa_error_log(st, NULL);
         }
 
-        fa_thread_sleep(3000);
+        fa_thread_sleep(300000);
         fa_destroy(st);
         fa_destroy(s);
     }
@@ -118,7 +163,7 @@ int main(int argc, char const *argv[])
     fa_set_log_std();
     fa_initialize();
 
-    run_clicks();
+    run_level();
 
     fa_terminate();
 }
