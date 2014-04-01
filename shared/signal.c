c63ee5c80400c771c3065e5599b8e14a587e8356
Hans Höglund <hans@hanshoglund.se>
2014-04-01 12:29:45 +0200

    Free synth graph (including external procs etc) properly #189
    - Do not destroy if no destroy function present

--- shared/signal.c
+++ shared/signal.c
@@ -862,7 +862,7 @@ void run_custom_procs(custom_proc_when_t when, int count, state_t state)
             break;
 
         case custom_proc_destroy: {
-            if (proc->data) {
+            if (proc->destroy && proc->data) {
                 proc->destroy(proc->data);
                 proc->data = NULL;
             }
