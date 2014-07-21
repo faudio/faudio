3adc2e31be9afb0aff10b36d678076195210af5b
Hans Höglund <hans@hanshoglund.se>
2014-07-21 11:30:39 +0200

    Clean repo

--- start-server.sh
+++
@@ -1,9 +0,0 @@
-
-# A server to test streaming
-#
-# ./start-server.sh
-# curl http://localhost:2233/test/test.wav
-# ./stop-server-sh
-
-(python -m SimpleHTTPServer 2233)>/dev/null 2>&1 & echo "kill $$;" > stop-server.sh &
-chmod a+x stop-server.sh
