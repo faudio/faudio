43a12ec73f963a6426dda871b5f73f4dc9f93f16
ErikR <erik.ronstrom@doremir.com>
2015-08-14 12:47:42 +0200

    Temporarily disable sysex for fluidsynth

--- platform/windows/signal.c
+++ platform/windows/signal.c
@@ -86,10 +86,10 @@ fa_ptr_t receive_(fa_ptr_t x, fa_signal_name_t n, fa_signal_message_t msg)
     if (fa_equal(n, fa_string("fluid"))) {
 
         if (!fa_midi_message_is_simple(msg)) {
-			//fa_warn(fa_string("SYSEX message to Fluidsynth"));
-			if (FLUID_OK != fluid_synth_sysex(synth, msg->sysex->data, msg->sysex->size, NULL, 0, NULL, 0)) {
-				fa_warn(fa_string("Fluidsynth: Could not send SYSEX message"));
-			}
+			fa_warn(fa_string("SYSEX message to Fluidsynth"));
+			//if (FLUID_OK != fluid_synth_sysex(synth, msg->sysex->data, msg->sysex->size, NULL, 0, NULL, 0)) {
+			//	fa_warn(fa_string("Fluidsynth: Could not send SYSEX message"));
+			//}
 			
         } else {
 
