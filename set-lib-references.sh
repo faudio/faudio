7d1ee5930a41a2ed07e5cbda13b883cb213b6aae
Erik Ronström <erik.ronstrom@doremir.com>
2017-09-17 13:12:10 +0200

    Bumped version number to 2.23.0

--- set-lib-references.sh
+++ set-lib-references.sh
@@ -9,9 +9,9 @@ install_name_tool -change /Volumes/DoReMIR/faudio/external/curl/source/../result
 install_name_tool -change /Volumes/DoReMIR/faudio/external/mpg123/source/../result/lib/libmpg123.0.dylib @executable_path/../Frameworks/libmpg123.0.dylib bin/faudio-server
 
 # Reference to Faudio.framework in faudio-server
-install_name_tool -change /Volumes/DoReMIR/faudio/build/Frameworks/Faudio.framework/Versions/2.20.2/Faudio @executable_path/../Frameworks/Faudio.framework/Versions/2.20.2/Faudio bin/faudio-server
+install_name_tool -change /Volumes/DoReMIR/faudio/build/Frameworks/Faudio.framework/Versions/2.23.0/Faudio @executable_path/../Frameworks/Faudio.framework/Versions/2.23.0/Faudio bin/faudio-server
 
 
 # Other
 install_name_tool -change /Volumes/DoReMIR/faudio/external/liblo/source/../result/lib/liblo.7.dylib @executable_path/../lib/liblo.7.dylib bin/faudio-io_simple_filter
-install_name_tool -change /Volumes/DoReMIR/faudio/build/Frameworks/Faudio.framework/Versions/2.20.2/Faudio @executable_path/../lib/Faudio.framework/Versions/2.20.2/Faudio bin/faudio-io_simple_filter
+install_name_tool -change /Volumes/DoReMIR/faudio/build/Frameworks/Faudio.framework/Versions/2.23.0/Faudio @executable_path/../lib/Faudio.framework/Versions/2.23.0/Faudio bin/faudio-io_simple_filter
